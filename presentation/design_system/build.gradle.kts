efe6c6a169bbdd75b146cf9d45af30a4da187373
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-06-29 21:59:40 +0300

    refactor: remove empty spaces

--- presentation/design_system/build.gradle.kts
+++ presentation/design_system/build.gradle.kts
@@ -33,15 +33,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(libs.androidx.material3)
     implementation(libs.androidx.activity.compose)
-
 }
