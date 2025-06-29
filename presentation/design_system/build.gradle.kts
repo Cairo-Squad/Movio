ca8dd26726cf1bc620c0dd800fdda5f19c40faa2
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-06-29 21:54:30 +0300

    feat: add material 3 to the design system module

--- presentation/design_system/build.gradle.kts
+++ presentation/design_system/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose)
+
 }
