21d84e80637984e477099e3b6a3b782e407d20ea
Zyad <zyadhammad531@gmail.com>
2025-06-30 22:22:27 +0300

    refactor: flatten module structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,10 +21,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "Movio"
 include(":app")
-include(":presentation:ui")
-include(":presentation:design_system")
-include(":presentation:viewmodel")
-include(":domain:usecase")
-include(":domain:entity")
-include(":data:repository")
-include(":data:remote")
+include(":ui")
+include(":design_system")
+include(":viewmodel")
+include(":usecase")
+include(":entity")
+include(":repository")
+include(":remote")
