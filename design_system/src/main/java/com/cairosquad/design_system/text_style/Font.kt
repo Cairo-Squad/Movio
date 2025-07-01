d2b7dd33f208284f18e897ea8e92f0a3f407241c
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-07-01 07:41:05 +0300

    feat: add Inter font family to design system

---
+++ design_system/src/main/java/com/cairosquad/design_system/text_style/Font.kt
@@ -0,0 +1,12 @@
+package com.cairosquad.design_system.text_style
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.cairosquad.design_system.R
+
+val Inter= FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_regular, FontWeight.Normal)
+)
