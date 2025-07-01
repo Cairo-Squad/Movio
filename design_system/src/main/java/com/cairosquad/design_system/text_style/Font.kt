77bfdf31df516ed2fdcec9c3173072972c8ae372
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-07-01 21:28:11 +0300

    refactor : improve formatting in Font.kt

--- design_system/src/main/java/com/cairosquad/design_system/text_style/Font.kt
+++ design_system/src/main/java/com/cairosquad/design_system/text_style/Font.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.cairosquad.design_system.R
 
-val Inter= FontFamily(
+val Inter = FontFamily(
     Font(R.font.inter_bold, FontWeight.Bold),
     Font(R.font.inter_medium, FontWeight.Medium),
     Font(R.font.inter_regular, FontWeight.Normal)
