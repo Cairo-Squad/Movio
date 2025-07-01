afbb7323565fa52b0854d385915060418b838db8
Menna Srour <mennasrour441@gmail.com>
2025-07-01 16:26:05 +0300

    feat: add ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,4 +6,31 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+}
+
+kover {
+    reports {
+        filters {
+            excludes {
+                classes(
+                    "**.model.**",
+                    "**.di.**",
+                    "**.exception.**",
+                    "**.mapper.**",
+                    "**.ui.**",
+                    "**.design_system.**",
+                    "**.entity.**",
+                )
+            }
+        }
+
+        total {
+            verify {
+                rule {
+                    minBound(80)
+                }
+            }
+        }
+    }
 }
--- usecase/build.gradle.kts
+++ usecase/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("org.jetbrains.kotlinx.kover")
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
