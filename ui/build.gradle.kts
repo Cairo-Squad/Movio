0694088a0d16695ca265961071c96a8841d24082
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-02 14:10:18 +0300

    refactor : reuse material3 dependency

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
@@ -50,6 +51,7 @@ dependencies {
     implementation(libs.androidx.foundation.layout.android)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.material)
+    implementation(libs.androidx.material3.android)
     implementation(libs.androidx.ui.tooling.preview.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
