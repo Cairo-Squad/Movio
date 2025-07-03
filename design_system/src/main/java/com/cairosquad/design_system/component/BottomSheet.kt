25687685e8b36542469d9a69f06150b1f27edaa7
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-07-03 18:38:02 +0300

    refactor: put Column scope to the content lambda function parameter in the bottom sheet, and improve the preview

--- design_system/src/main/java/com/cairosquad/design_system/component/BottomSheet.kt
+++ design_system/src/main/java/com/cairosquad/design_system/component/BottomSheet.kt
@@ -2,11 +2,9 @@ package com.cairosquad.design_system.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,20 +19,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cairosquad.design_system.theme.MovioTheme
 import com.cairosquad.design_system.theme.Theme
-/**
- * Displays a customizable modal bottom sheet.
- *
- * @param isVisible Controls the visibility of the bottom sheet.
- * @param onDismiss Callback invoked when clicking outside the bottom sheet.
- * @param modifier Modifier to be applied to the bottom sheet.
- * @param containerColor Background color of the bottom sheet, equal to surface by default .
- * @param content Composable content to be displayed inside the sheet.
- */
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheet(
@@ -42,7 +34,7 @@ fun BottomSheet(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
     containerColor: Color = Theme.color.surfaces.surface,
-    content: @Composable () -> Unit
+    content: @Composable ColumnScope.() -> Unit
 ) {
     val sheetState = rememberModalBottomSheetState()
     var modifiedIsVisible by remember { mutableStateOf(isVisible) }
@@ -68,65 +60,69 @@ fun BottomSheet(
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-private fun BottomSheetContentPreview() {
+private fun BottomSheetLightPreview(){
+    BottomSheetPreview(isDarkTheme = false)
+}
+
+@Preview
+@Composable
+private fun BottomSheetDarkPreview(){
+    BottomSheetPreview(isDarkTheme = true)
+}
+
+@Composable
+private fun BottomSheetPreview(isDarkTheme: Boolean = false) {
 
     var isVisible = remember { mutableStateOf(false) }
 
-    MovioTheme {
+    MovioTheme(
+        isDarkTheme = isDarkTheme
+    ) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Theme.color.surfaces.surface),
             contentAlignment = Alignment.Center
         ) {
-            Box(
+            Text(
+                text = "Show Bottom Sheet",
                 modifier = Modifier
                     .background(Theme.color.brand.primary, CircleShape)
+                    .clip(CircleShape)
                     .clickable { isVisible.value = true }
                     .padding(12.dp),
+                style = Theme.textStyle.label.smallRegular12,
+                color = Theme.color.brand.onPrimary,
+                textAlign = TextAlign.Center
+            )
 
-            contentAlignment = Alignment.Center
-            ){
-                Text(
-                    text = "Show Bottom Sheet",
-                    modifier = Modifier.align(Alignment.Center),
-                    style = Theme.textStyle.label.smallRegular12,
-                    color = Theme.color.surfaces.onSurface
-                )
-            }
             BottomSheet(
                 isVisible = isVisible.value,
-                onDismiss = { isVisible.value = false },
+                onDismiss = { isVisible.value = false }
             ) {
-                Column(
+                Text(
+                    text = "Custom Bottom Sheet Content",
+                    style = Theme.textStyle.title.largeBold16,
+                    color = Theme.color.surfaces.onSurface,
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(12.dp)
+                )
+                Text(
+                    text = "Show Bottom Sheet",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(12.dp)
+                        .background(Theme.color.brand.primary, CircleShape)
+                        .clip(CircleShape)
+                        .clickable { isVisible.value = false }
                         .padding(12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    Text(
-                        text = "Custom Bottom Sheet Content",
-                        style = Theme.textStyle.title.largeBold16,
-                        color = Theme.color.surfaces.onSurface
-                    )
-                    Box(
-                        modifier = Modifier
-                            .background(Theme.color.brand.primary, CircleShape)
-                            .clickable { isVisible.value = false }
-                            .padding(12.dp),
-                        contentAlignment = Alignment.Center
-                    ){
-                        Text(
-                            text = "hide Bottom Sheet",
-                            modifier = Modifier.align(Alignment.Center),
-                            style = Theme.textStyle.label.smallRegular12,
-                            color = Theme.color.surfaces.onSurface
-                        )
-                    }
-                }
+                    style = Theme.textStyle.label.smallRegular12,
+                    color = Theme.color.brand.onPrimary,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
