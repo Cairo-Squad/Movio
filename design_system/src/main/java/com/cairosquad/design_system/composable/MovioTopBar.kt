f610cc9f0c86d7ad314e0b25554ccb269841787f
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-03 16:56:15 +0300

    refactor: move implementation to design system

--- design_system/src/main/java/com/cairosquad/design_system/composable/MovioTopBar.kt
+++ design_system/src/main/java/com/cairosquad/design_system/composable/MovioTopBar.kt
@@ -1,4 +1,4 @@
-package com.cairosquad.ui
+package com.cairosquad.design_system.composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -56,13 +56,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.foundation.layout.android)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.material3.android)
     implementation(project(":design_system"))
     implementation(project(":viewmodel"))
 }
