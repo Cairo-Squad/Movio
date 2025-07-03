22b168b122bd2b0ba48407c32e008064358c02e7
NadeenXo <nadeenserag@gmail.com>
2025-07-03 18:11:22 +0300

    add img placeholder

--- ui/src/main/java/com/cairosquad/ui/composable/ArtistCard.kt
+++ ui/src/main/java/com/cairosquad/ui/composable/ArtistCard.kt
@@ -1,35 +1,28 @@
 package com.cairosquad.ui.composable
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cairosquad.design_system.text_style.TitleTextStyle
 import com.cairosquad.design_system.theme.MovioTheme
 import com.cairosquad.design_system.theme.Theme
 import com.cairosquad.ui.R
 
 @Composable
-fun ArtistCard(name: String = "unknown", imgRes: Int = R.drawable.search_bottom_nav_colored) {
+fun ArtistCard(name: String, imgRes: Int) {
     Column(
         modifier = Modifier
             .widthIn(max = 102.dp)
@@ -63,6 +56,6 @@ fun ArtistCard(name: String = "unknown", imgRes: Int = R.drawable.search_bottom_
 @Composable
 private fun ArtistCardPreview() {
     MovioTheme {
-        ArtistCard()
+        ArtistCard(name = "Ana de Armas", imgRes = R.drawable.artist)
     }
 }
