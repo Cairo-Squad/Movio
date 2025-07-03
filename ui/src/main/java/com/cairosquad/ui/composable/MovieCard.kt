ee273d4677a6f9c1931ef3d0d485011552a2c965
NadeenXo <nadeenserag@gmail.com>
2025-07-03 18:56:43 +0300

    added img description, changed icon style, added overflow text

--- ui/src/main/java/com/cairosquad/ui/composable/MovieCard.kt
+++ ui/src/main/java/com/cairosquad/ui/composable/MovieCard.kt
@@ -4,25 +4,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,12 +45,24 @@ fun MovieCard(
         ) {
             Image(
                 painter = painterResource(id = imgRes),
-                contentDescription = null,
-                modifier = Modifier.heightIn(max = 136.dp)
+                contentDescription = stringResource(R.string.movie_poster),
+                modifier = Modifier
+                    .heightIn(max = 136.dp)
                     .fillMaxWidth()
                     .clip(RoundedCornerShape(5.dp)),
                 contentScale = ContentScale.Crop
             )
+//            todo: change if added coil
+//            AsyncImage(
+//                model = imgRes,
+//                contentDescription = stringResource(R.string.movie_poster),
+//                modifier = Modifier
+//                .heightIn(max = 136.dp)
+//                .fillMaxWidth()
+//                .clip(RoundedCornerShape(5.dp)),
+//                contentScale = ContentScale.Crop,
+//                error = painterResource(id = R.drawable.movie_card_img)
+//            )
 
             Row(
                 modifier = Modifier
@@ -61,16 +71,18 @@ fun MovieCard(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
-                    imageVector = Icons.Default.Star,
+                    modifier = Modifier
+                        .size(16.dp)
+                        .padding(end = 4.dp),
+                    painter = painterResource(id = R.drawable.star),
                     contentDescription = "Rating Star",
-                    tint = Color.Yellow,
-                    modifier = Modifier.size(16.dp)
+                    tint = Theme.color.system.warning,
                 )
-                Spacer(modifier = Modifier.padding(end = 1.dp))
+
                 Text(
                     text = rating,
                     fontSize = 10.sp,
-                    color = Color.Yellow,
+                    color = Theme.color.system.warning,
                     style = Theme.textStyle.label.smallRegular12
                 )
             }
@@ -82,7 +94,8 @@ fun MovieCard(
                 .padding(top = 8.dp),
             style = Theme.textStyle.title.mediumMedium14,
             textAlign = TextAlign.Start,
-            maxLines = 2
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
