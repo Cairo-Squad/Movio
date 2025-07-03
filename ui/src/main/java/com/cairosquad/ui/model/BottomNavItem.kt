e2a3373a6598600aa81fc122dfb0800f50541ff7
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-03 22:44:22 +0300

    refactor : remove unnecessary files

--- ui/src/main/java/com/cairosquad/ui/model/BottomNavItem.kt
+++
@@ -1,9 +0,0 @@
-package com.cairosquad.ui.model
-
-import androidx.annotation.DrawableRes
-
-data class BottomNavItem(
-    @DrawableRes val unColoredIcon: Int,
-    @DrawableRes val coloredIcon: Int,
-    val label: String
-)
