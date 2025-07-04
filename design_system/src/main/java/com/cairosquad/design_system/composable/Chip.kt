a596578f8f42d6417b2c122301b798cd68bda1c6
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-04 13:05:36 +0300

    refactor: make the chip clickable

--- design_system/src/main/java/com/cairosquad/design_system/composable/Chip.kt
+++ design_system/src/main/java/com/cairosquad/design_system/composable/Chip.kt
@@ -1,23 +1,27 @@
 package com.cairosquad.design_system.composable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cairosquad.design_system.theme.MovioTheme
 import com.cairosquad.design_system.theme.Theme
 
 @Composable
-fun MovioChip(
+fun Chip(
     title: String,
     modifier: Modifier = Modifier,
     isSelected: Boolean = false,
+    onClick: () -> Unit = {}
 ) {
     val backgroundColor =
         if (isSelected) Theme.color.brand.primary else Theme.color.surfaces.onSurfaceAt3
@@ -26,8 +30,10 @@ fun MovioChip(
 
     Box(
         modifier = modifier
-            .background(color = backgroundColor, shape = RoundedCornerShape(24.dp))
-            .padding(horizontal = 12.dp, vertical = 8.dp)
+            .background(color = backgroundColor, shape = CircleShape)
+            .clip(CircleShape)
+            .padding(horizontal = 12.dp, vertical = 7.5.dp)
+            .clickable(onClick = onClick)
     ) {
         Text(
             modifier = Modifier.align(Alignment.Center),
@@ -40,8 +46,8 @@ fun MovioChip(
 
 @Preview
 @Composable
-private fun MovioChipPreview() {
+private fun ChipPreview() {
     MovioTheme {
-        MovioChip(title = "All", isSelected = true)
+        Chip(title = "All", isSelected = true)
     }
 }
