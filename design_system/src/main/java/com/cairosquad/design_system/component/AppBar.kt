535a9c0db1c58947028f0f7a966202f3ca0ff2e2
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-07-08 20:06:16 +0300

    fix: center the title in the app bar, resolve the text overflow issue, and add more cases to the preview

--- design_system/src/main/java/com/cairosquad/design_system/component/AppBar.kt
+++ design_system/src/main/java/com/cairosquad/design_system/component/AppBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,12 +21,13 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.cairosquad.design_system.R
 import com.cairosquad.design_system.theme.MovioTheme
 import com.cairosquad.design_system.theme.Theme
-import com.cairosquad.design_system.R
-
 
 @Composable
 fun AppBar(
@@ -35,7 +37,7 @@ fun AppBar(
     onShareButtonClicked: (() -> Unit)? = null,
     onFavoriteButtonClicked: (() -> Unit)? = null,
 ) {
-    Row(
+    Box(
         modifier = modifier
             .fillMaxWidth()
             .height(48.dp)
@@ -59,22 +61,33 @@ fun AppBar(
             )
         }
 
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .align(Alignment.CenterVertically)
-        ) {
-            if (title != null) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = title,
-                    color = Theme.color.surfaces.onSurface,
-                    style = Theme.textStyle.headline.largeBold16,
-                )
-            }
+        val titleHorizontalPadding =
+            if (onShareButtonClicked != null && onFavoriteButtonClicked != null) 80
+            else if (
+                onShareButtonClicked != null
+                || onFavoriteButtonClicked != null
+                || onBackButtonClicked != null
+            ) 40
+            else 0
+
+        if (title != null) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(horizontal = titleHorizontalPadding.dp),
+                text = title,
+                color = Theme.color.surfaces.onSurface,
+                style = Theme.textStyle.headline.largeBold16,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+            )
         }
 
-        Row(horizontalArrangement = Arrangement.spacedBy(4.dp))
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        )
         {
             if (onShareButtonClicked != null) {
                 Icon(
@@ -109,13 +122,41 @@ fun AppBar(
 @Composable
 private fun MovioAppBarPreview() {
     MovioTheme {
-        Box(Modifier.background(Theme.color.surfaces.surface))
+        Column(
+            Modifier.background(Theme.color.surfaces.surface),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        )
         {
             AppBar(
-                title = "nour",
+                title = "Title",
+                onBackButtonClicked = null,
+                onShareButtonClicked = null,
+                onFavoriteButtonClicked = null
+            )
+            AppBar(
+                title = "Title",
+                onBackButtonClicked = {},
+                onShareButtonClicked = null,
+                onFavoriteButtonClicked = {}
+            )
+            AppBar(
+                title = "Title",
+                onBackButtonClicked = null,
+                onShareButtonClicked = {},
+                onFavoriteButtonClicked = null
+            )
+            AppBar(
+                title = "Title",
+                onBackButtonClicked = {},
+                onShareButtonClicked = null,
+                onFavoriteButtonClicked = null
+            )
+            AppBar(
+                title = "Title",
                 onBackButtonClicked = {},
                 onShareButtonClicked = {},
-                onFavoriteButtonClicked = {})
+                onFavoriteButtonClicked = {}
+            )
         }
     }
 }
