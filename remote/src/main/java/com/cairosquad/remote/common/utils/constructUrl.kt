60974f042e69b00ea6d8e02ac959e7a2622c8314
youssef mohammed <youssef.mu.saber@gmail.com>
2025-07-09 22:37:28 +0300

    feature: added api key secret in the buildConfig.

--- remote/src/main/java/com/cairosquad/remote/common/utils/constructUrl.kt
+++ remote/src/main/java/com/cairosquad/remote/common/utils/constructUrl.kt
@@ -1,10 +1,11 @@
 package com.cairosquad.remote.common.utils
 
+import com.cairosquad.remote.BuildConfig
 import com.cairosquad.remote.common.Constants
 
 fun constructUrl(url: String): String {
     return when {
-        url.contains(Constants.BASE_URL) -> url
+        url.contains(BuildConfig.API_KEY) -> url
         url.startsWith("/") -> Constants.BASE_URL + url.drop(1)
         else -> Constants.BASE_URL + url
     }
