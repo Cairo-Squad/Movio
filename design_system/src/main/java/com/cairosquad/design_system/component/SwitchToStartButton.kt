fd43165089bfd7c45c53114eed87c1628003a90f
Menna Srour <mennasrour441@gmail.com>
2025-07-09 23:58:35 +0300

    fix: add unresolved plugins to version catalog

--- design_system/src/main/java/com/cairosquad/design_system/component/SwitchToStartButton.kt
+++ design_system/src/main/java/com/cairosquad/design_system/component/SwitchToStartButton.kt
@@ -37,12 +37,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.ExperimentalWearMaterialApi
 import com.cairosquad.design_system.R
 import com.cairosquad.design_system.theme.Theme
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalWearMaterialApi::class)
 @Composable
 fun StartSwipeButton(
     modifier: Modifier = Modifier,
