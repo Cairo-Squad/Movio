28ed429b1ae697c0c86033d5eaaf0b59df9515ed
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-10 21:50:30 +0300

    remove unused imports

--- domain/src/main/java/com/cairosquad/domain/search/usecase/GetForYouUseCase.kt
+++ domain/src/main/java/com/cairosquad/domain/search/usecase/GetForYouUseCase.kt
@@ -1,7 +1,6 @@
 package com.cairosquad.domain.usecase
 
 import com.cairosquad.domain.search.repository.ForURepository
-import com.cairosquad.domain.search.repository.SearchRepository
 import com.cairosquad.entity.Movie
 
 class GetForYouUseCase(  private val foryouRepository: ForURepository,) {
