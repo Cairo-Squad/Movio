0bdfba7c4007e8211fa80991e8ea2773c38ebf79
youssef mohammed <youssef.mu.saber@gmail.com>
2025-07-10 15:36:38 +0300

    feature: created loading movie card.

---
+++ design_system/src/main/java/com/cairosquad/design_system/component/LoadingMovieCard.kt
@@ -0,0 +1,105 @@
+package com.cairosquad.design_system.component
+
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.cairosquad.design_system.preview.MultiThemePreviews
+import com.cairosquad.design_system.theme.MovioTheme
+import com.cairosquad.design_system.theme.Theme
+
+@Composable
+fun LoadingMovieCard(modifier: Modifier = Modifier) {
+    val shimmerColors = listOf(
+        Color.Transparent,
+        Theme.color.surfaces.surface,
+        Color.Transparent
+    )
+
+    val infiniteTransition = rememberInfiniteTransition(label = "Loading Animation")
+
+    val shimmerEffectAnimation by infiniteTransition.animateFloat(
+        initialValue = -200f,
+        targetValue = 500f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 2000, easing = EaseInOut),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset(shimmerEffectAnimation, shimmerEffectAnimation),
+        end = Offset(shimmerEffectAnimation + 190f, shimmerEffectAnimation + 190f)
+    )
+
+    Box(modifier = modifier) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .background(Theme.color.surfaces.surface)
+                .width(101.33.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(136.dp)
+                    .fillMaxWidth()
+                    .background(
+                        Theme.color.surfaces.surfaceContainer,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .height(15.dp)
+                    .fillMaxWidth()
+                    .background(Theme.color.surfaces.surfaceContainer, shape = CircleShape)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .height(159.dp)
+                .fillMaxWidth()
+                .background(brush)
+        )
+    }
+}
+
+@MultiThemePreviews
+@Composable
+private fun LoadingCardPreview() {
+    MovioTheme {
+        LazyVerticalGrid(
+            modifier = Modifier.background(Theme.color.surfaces.surface),
+            columns = GridCells.Fixed(3),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp)
+        ) {
+            items(20) {
+                LoadingMovieCard()
+            }
+        }
+    }
+}
