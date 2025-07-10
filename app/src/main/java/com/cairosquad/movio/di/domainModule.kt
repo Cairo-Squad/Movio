d06836f9637dd0ad48933ee071152d90007b83d2
Zyad <zyadhammad531@gmail.com>
2025-07-11 00:29:26 +0300

    refactor: use single of instead of single in di

--- app/src/main/java/com/cairosquad/movio/di/domainModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/domainModule.kt
@@ -3,18 +3,11 @@ package com.cairosquad.movio.di
 import com.cairosquad.domain.search.usecase.GetExploreMoreUseCase
 import com.cairosquad.domain.search.usecase.GetForYouUseCase
 import com.cairosquad.domain.search.usecase.SearchUseCase
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val domainModule = module {
-    single {
-        SearchUseCase(get(), get())
-    }
-
-    single {
-        GetForYouUseCase(get())
-    }
-
-    single {
-        GetExploreMoreUseCase(get())
-    }
+    singleOf(::SearchUseCase)
+    singleOf(::GetForYouUseCase)
+    singleOf(::GetExploreMoreUseCase)
 }
