7dfbdf0744a5f320bc49e3d66bde4cb625350331
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-10 23:11:16 +0300

    resolve conflicts

--- domain/src/main/java/com/cairosquad/domain/search/usecase/GetForYouUseCase.kt
+++ domain/src/main/java/com/cairosquad/domain/search/usecase/GetForYouUseCase.kt
@@ -1,6 +1,6 @@
 package com.cairosquad.domain.usecase
 
-import com.cairosquad.domain.search.repository.ForURepository
+import com.cairosquad.domain.search.repository.MovieRepository
 import com.cairosquad.entity.Movie
 
 class GetForYouUseCase(  private val movieRepository: MovieRepository,) {
