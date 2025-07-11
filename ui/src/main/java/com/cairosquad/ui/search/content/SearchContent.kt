0d6fd6c02740b1e4e96336b7488c024d412ff4eb
youssef mohammed <youssef.mu.saber@gmail.com>
2025-07-11 06:37:01 +0300

    refactor: extracted RecentSearchItem to composable.

--- ui/src/main/java/com/cairosquad/ui/search/content/SearchContent.kt
+++ ui/src/main/java/com/cairosquad/ui/search/content/SearchContent.kt
@@ -2,39 +2,27 @@ package com.cairosquad.ui.search.content
 
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.cairosquad.design_system.R
 import com.cairosquad.design_system.component.InputField
+import com.cairosquad.design_system.component.RecentSearchItem
 import com.cairosquad.design_system.component.SectionHeader
 import com.cairosquad.design_system.theme.Theme
 import com.cairosquad.viewmodel.searchviewmodel.SearchInteractionListener
@@ -93,60 +81,17 @@ fun SearchContent(
         )
         LazyColumn(
             modifier = Modifier.padding(horizontal = 16.dp),
-            contentPadding = PaddingValues(vertical = 12.dp)
+            contentPadding = PaddingValues(vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
             items(state.recentSearch) {
                 RecentSearchItem(
                     recentItem = it,
+                    query = state.query,
                     onRemoveHistoryItem = listener::onRemoveHistoryItem,
                     onRecentSearchItemClicked = listener::onRecentSearchItemClicked
                 )
             }
         }
-    }
-}
-
-@Composable
-private fun RecentSearchItem(
-    modifier: Modifier = Modifier,
-    recentItem: String,
-    onRecentSearchItemClicked: (String) -> Unit,
-    onRemoveHistoryItem: (String) -> Unit
-) {
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .weight(1f)
-                .clip(RoundedCornerShape(8.dp))
-                .clickable(onClick = { onRecentSearchItemClicked(recentItem) }),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Image(
-                modifier = Modifier
-                    .size(24.dp),
-                imageVector = ImageVector.vectorResource(com.cairosquad.ui.R.drawable.recent),
-                contentDescription = stringResource(com.cairosquad.ui.R.string.recent_item_icon),
-                colorFilter = ColorFilter.tint(Theme.color.surfaces.onSurfaceVariant)
-            )
-            BasicText(
-                modifier = Modifier.weight(1f),
-                text = recentItem,
-                style = Theme.textStyle.label.smallRegular14.copy(color = Theme.color.surfaces.onSurfaceVariant)
-            )
-        }
-        Image(
-            modifier = Modifier
-                .clip(CircleShape)
-                .clickable(onClick = { onRemoveHistoryItem(recentItem) })
-                .size(24.dp),
-            imageVector = ImageVector.vectorResource(R.drawable.ic_close),
-            contentDescription = stringResource(com.cairosquad.ui.R.string.clear_search_result),
-            colorFilter = ColorFilter.tint(Theme.color.surfaces.onSurfaceVariant)
-        )
     }
 }
