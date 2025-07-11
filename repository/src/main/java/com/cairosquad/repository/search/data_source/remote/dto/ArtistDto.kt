3034aea85ac7d3519d6009924fe8fac540f40ac6
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-07-11 10:58:09 +0300

    test: make empty test file for http client factory test, and remove unnecessary attributes from dto

--- repository/src/main/java/com/cairosquad/repository/search/data_source/remote/dto/ArtistDto.kt
+++ repository/src/main/java/com/cairosquad/repository/search/data_source/remote/dto/ArtistDto.kt
@@ -7,24 +7,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ArtistDto(
-    @SerialName("adult")
-    val adult: Boolean? = null,
-    @SerialName("gender")
-    val gender: Int? = null,
     @SerialName("id")
     val id: Int? = null,
-    @SerialName("known_for_department")
-    val knownForDepartment: String? = null,
     @SerialName("name")
     val name: String? = null,
-    @SerialName("original_name")
-    val originalName: String? = null,
-    @SerialName("popularity")
-    val popularity: Double? = null,
     @SerialName("profile_path")
     val profilePath: String? = null,
-    @SerialName("known_for")
-    val knownFor: List<MediaDto?>? = null
 ) {
     fun toArtist(): Artist {
         return Artist(
