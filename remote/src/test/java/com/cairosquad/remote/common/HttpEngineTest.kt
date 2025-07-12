f4cb129f5c3ccd20807a57cf7ea19005098cd9cd
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-12 13:37:29 +0300

    test : add http client factory and http engine test with coverage 100%

--- remote/src/test/java/com/cairosquad/remote/common/HttpEngineTest.kt
+++ remote/src/test/java/com/cairosquad/remote/common/HttpEngineTest.kt
@@ -1,5 +1,17 @@
 package com.cairosquad.remote.common
 
-class HttpEngineTest {
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttpEngine
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.junit.Assert.*
+import org.junit.Test
+import java.lang.reflect.Field
 
+class HttpEngineTest {
+    @Test
+    fun provide_should_return_OkHttpEngine() {
+        val engine = HttpEngine.provide()
+        assertTrue(engine is OkHttpEngine)
+    }
 }
