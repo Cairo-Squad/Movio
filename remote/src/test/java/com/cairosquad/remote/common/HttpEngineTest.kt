6557d818f65fc6f1082101e4c4966bb35c7bc22e
Zyad <zyadhammad531@gmail.com>
2025-07-14 11:44:01 +0300

    refactor: http engine test name

--- remote/src/test/java/com/cairosquad/remote/common/HttpEngineTest.kt
+++ remote/src/test/java/com/cairosquad/remote/common/HttpEngineTest.kt
@@ -1,16 +1,12 @@
 package com.cairosquad.remote.common
 
-import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttpEngine
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.junit.Assert.*
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.lang.reflect.Field
 
 class HttpEngineTest {
     @Test
-    fun provide_should_return_OkHttpEngine() {
+    fun `provide should return OkHttpEngine`() {
         val engine = HttpEngine.provide()
         assertTrue(engine is OkHttpEngine)
     }
