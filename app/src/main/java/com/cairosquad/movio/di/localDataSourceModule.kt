5e0d4f9bdc1824ab8d5510874fb608d5e9e1cd00
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-14 19:59:47 +0300

    update localDataSourceModule

--- app/src/main/java/com/cairosquad/movio/di/localDataSourceModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/localDataSourceModule.kt
@@ -2,10 +2,12 @@ package com.cairosquad.movio.di
 
 import androidx.room.Room
 import com.cairosquad.local.common.MovioDataBase
+import com.cairosquad.local.search.cache.LocalMovieDiscoveryCacheDataSourceImpl
 import com.cairosquad.local.search.cache.LocalSearchCacheDataSourceImpl
 import com.cairosquad.local.search.cache.dao.CacheDao
 import com.cairosquad.local.search.recent.LocalRecentSearchDataSourceImpl
 import com.cairosquad.local.search.recent.dao.LocalRecentSearchDao
+import com.cairosquad.repository.search.data_source.local.LocalMovieDiscoveryCacheDataSource
 import com.cairosquad.repository.search.data_source.local.LocalRecentSearchDataSource
 import com.cairosquad.repository.search.data_source.local.LocalSearchCacheDataSource
 import org.koin.android.ext.koin.androidContext
@@ -30,5 +32,9 @@ val localDataSourceModule = module {
 
     single<LocalRecentSearchDataSource> {
         LocalRecentSearchDataSourceImpl(get())
+    }
+
+    single<LocalMovieDiscoveryCacheDataSource> {
+        LocalMovieDiscoveryCacheDataSourceImpl(get())
     }
 }
