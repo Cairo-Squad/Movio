50bb4efb26bc505bd85f04a2e4b3a4acb9b70517
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-15 12:13:46 +0300

    refactor : use safer image viewer

--- ui/src/main/java/com/cairosquad/ui/movio_component/ReviewCard.kt
+++ ui/src/main/java/com/cairosquad/ui/movio_component/ReviewCard.kt
@@ -1,12 +1,10 @@
 package com.cairosquad.ui.movio_component
 
-import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,22 +18,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cairosquad.design_system.preview.MultiThemePreviews
-import com.cairosquad.design_system.theme.MovioTheme
 import com.cairosquad.design_system.theme.Theme
+import com.cairosquad.safe_image_viewer.safe_image_viewer.SafeImageViewer
 import com.cairosquad.ui.R
 
 @Composable
 fun ReviewCard(
-    reviewerImage: Painter,
+    imgUrl: String,
     movieTitle: String,
     rating: String,
     reviewDate: String,
@@ -57,14 +51,31 @@ fun ReviewCard(
     ) {
         Row(
         ) {
-            Image(
-                modifier = Modifier
-                    .size(32.dp)
-                    .clip(CircleShape)
-                    .align(Alignment.CenterVertically),
-                painter = reviewerImage,
-                contentDescription = stringResource(R.string.reviewer_image)
-            )
+            if (imgUrl?.isNotEmpty() == true) {
+                SafeImageViewer(
+                    model = "https://image.tmdb.org/t/p/w500$imgUrl",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.CenterVertically),
+                    contentDescription = stringResource(R.string.reviewer_image),
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.CenterVertically)
+                        .background(Theme.color.system.defaultImageBackground),
+                    contentAlignment = Alignment.Center
+                ){
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = com.cairosquad.design_system.R.drawable.image_icon),
+                        contentDescription = stringResource(R.string.reviewer_image),
+                        tint = Color(0xFFEFF1F5)
+                    )
+                }
+            }
             Column(
                 modifier = Modifier
                     .padding(start = 8.dp)
@@ -106,36 +117,5 @@ fun ReviewCard(
             overflow = TextOverflow.Ellipsis
         )
 
-    }
-}
-
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-)
-@Composable
-private fun ReviewCardLightPreview() {
-    MovioTheme (isDarkTheme = false){
-        ReviewCard(
-            reviewerImage = painterResource(com.cairosquad.design_system.R.drawable.more_bottom_nav_colored),
-            movieTitle = "Awkwafina",
-            rating = "4.5",
-            reviewDate = "June 14, 2025",
-            reviewText = "This isn’t a film, it’s a live action video game with a predictable plot and loads of energetically choreographed CGI to substitute for anything vaguely akin to a story."
-        )
-    }
-}
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-)
-@Composable
-private fun ReviewCardNightPreview() {
-    MovioTheme (isDarkTheme = true){
-        ReviewCard(
-            reviewerImage = painterResource(com.cairosquad.design_system.R.drawable.more_bottom_nav_colored),
-            movieTitle = "Awkwafina",
-            rating = "4.5",
-            reviewDate = "June 14, 2025",
-            reviewText = "This isn’t a film, it’s a live action video game with a predictable plot and loads of energetically choreographed CGI to substitute for anything vaguely akin to a story."
-        )
     }
 }
