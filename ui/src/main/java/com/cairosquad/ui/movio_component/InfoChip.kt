7547d9695750c0dd3e579b884ea03cdd886bcc78
NadeenXo <nadeenserag@gmail.com>
2025-07-15 10:55:53 +0300

    change icon composable + create content description + fix text color

--- ui/src/main/java/com/cairosquad/ui/movio_component/InfoChip.kt
+++ ui/src/main/java/com/cairosquad/ui/movio_component/InfoChip.kt
@@ -1,20 +1,22 @@
 package com.cairosquad.ui.movio_component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cairosquad.design_system.R
@@ -30,31 +32,30 @@ fun InfoChip(
 ) {
     Row(
         modifier = modifier
+            .clip(CircleShape)
             .background(
                 color = Theme.color.surfaces.surfaceContainer,
-                shape = CircleShape
             )
             .border(
                 width = 1.dp, color = Theme.color.surfaces.onSurfaceAt3,
                 shape = CircleShape
             )
-            .clip(CircleShape)
             .clickable(onClick = onClick)
             .padding(horizontal = 12.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
+        Icon(
             modifier = Modifier
-                .padding(end = 4.dp)
-                .size(16.dp),
-            painter = painterResource(id = imgRes),
-            contentDescription = "Icon Info",
+                .padding(end = 4.dp),
+            imageVector = ImageVector.vectorResource(id = imgRes),
+            contentDescription = stringResource(com.cairosquad.ui.R.string.icon),
+            tint = Color.Unspecified
         )
         Text(
             modifier = Modifier
                 .padding(bottom = 1.dp),
             text = text,
-            color = Theme.color.surfaces.onSurfaceVariant,
+            color = Theme.color.surfaces.onSurfaceContainer,
             style = Theme.textStyle.label.smallRegular12,
         )
     }
