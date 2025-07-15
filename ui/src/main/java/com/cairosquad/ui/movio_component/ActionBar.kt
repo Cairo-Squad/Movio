eae027c343129d80ccfd9e04b6aa67d10a518e8a
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-07-15 11:46:20 +0300

    feat: add ActionBar component with icons and localized strings

---
+++ ui/src/main/java/com/cairosquad/ui/movio_component/ActionBar.kt
@@ -0,0 +1,90 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cairosquad.design_system.theme.Theme
+import com.cairosquad.ui.R
+
+@Composable
+fun ActionBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.outline_star),
+                contentDescription = "Rate it",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(24.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.rate),
+                color = Theme.color.surfaces.onSurfaceContainer,
+                style = Theme.textStyle.label.smallRegular12,
+                modifier = Modifier.padding(top = 4.dp)
+
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(46.67.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(Color(0xFFB7A4FB), Color(0xFF663EF6))
+                    ),
+                    shape = CircleShape
+                )
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.outline_play),
+                contentDescription = "Play",
+                tint = Color.Unspecified,
+            )
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.outline_bookmark),
+                contentDescription = "Add to list",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(24.dp)
+            )
+            Text(
+                text = stringResource(R.string.add_to_list),
+                color = Theme.color.surfaces.onSurfaceContainer,
+                style = Theme.textStyle.label.smallRegular12,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun lol() {
+    ActionBar()
+}
