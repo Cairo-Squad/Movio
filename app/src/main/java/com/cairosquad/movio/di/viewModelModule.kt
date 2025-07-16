c2b26b53b59beef6de5619b1641a068d9f7a3525
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-16 13:01:25 +0300

    refactor : support paging for artist and series

--- app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
@@ -1,9 +1,16 @@
 package com.cairosquad.movio.di
 
 import com.cairosquad.viewmodel.search.SearchViewModel
+import com.cairosquad.viewmodel.search.paging.ArtistPager
+import com.cairosquad.viewmodel.search.paging.MoviePager
+import com.cairosquad.viewmodel.search.paging.SeriesPager
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
+    singleOf(::MoviePager)
+    singleOf(::SeriesPager)
+    singleOf(::ArtistPager)
 }
