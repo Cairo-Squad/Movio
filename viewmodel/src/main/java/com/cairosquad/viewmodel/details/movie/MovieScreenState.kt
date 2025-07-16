53b1ddf391e7b23432c5d78e8b2e3f6177dcf92d
Zyad <zyadhammad531@gmail.com>
2025-07-17 00:26:02 +0300

    feature: add movie detail screen state

---
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/details/movie/MovieScreenState.kt
@@ -0,0 +1,67 @@
+package com.cairosquad.viewmodel.details.movie
+
+import com.cairosquad.entity.Genre
+
+data class MovieScreenState(
+    val isLoading: Boolean = true,
+    val movie: MovieDetailsUiState = MovieDetailsUiState(),
+    val topCast: List<TopCastUiState> = emptyList(),
+    val reviews: List<ReviewUiState> = emptyList(),
+    val similarMovies: List<SimilarMovieUiState> = emptyList(),
+    val isFavorite: Boolean = false,
+    val isShareBottomSheetOpen: Boolean = false,
+    val isNoAccountBottomSheetOpen: Boolean = false,
+    val isRateBottomSheetOpen: Boolean = false,
+    val isAddToListBottomSheetOpen: Boolean = false,
+    val successStatus: SuccessStatus = SuccessStatus.NONE,
+    val errorStatus: ErrorStatus = ErrorStatus.NONE
+) {
+    data class MovieDetailsUiState(
+        val id: Long = 0,
+        val title: String = "",
+        val rating: Float = 0.0f,
+        val posterPath: String = "",
+        val genres: List<Genre> = emptyList(),
+        val overview: String = "",
+        val releaseDate: Long = 0,
+        val runtimeMinutes: Int = 0,
+    )
+
+    data class TopCastUiState(
+        val id: Long = 0,
+        val name: String = "",
+        val photoPath: String = ""
+    )
+
+    data class ReviewUiState(
+        val id: Long = 0,
+        val author: String = "",
+        val authorPhotoPath: String = "",
+        val rating: Float = 0.0f,
+        val date: Long,
+        val description: String,
+    )
+
+    data class SimilarMovieUiState(
+        val id: Long = 0,
+        val title: String = "",
+        val rating: Float = 0.0f,
+        val posterPath: String = "",
+    )
+
+    enum class SuccessStatus {
+        NONE,
+        SHARE,
+        RATE,
+        ADD_TO_LIST,
+        // TODO: ADD?
+    }
+
+    enum class ErrorStatus {
+        NONE,
+        NO_INTERNET,
+        UNAUTHORIZED,
+        UNKNOWN,
+        // TODO: ADD?
+    }
+}
