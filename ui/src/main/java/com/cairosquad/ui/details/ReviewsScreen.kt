5446032b345ba1d1f49165af478d95a5cd15109d
Menna Srour <mennasrour441@gmail.com>
2025-07-17 14:53:00 +0300

    refactor: add system bar padding

--- ui/src/main/java/com/cairosquad/ui/details/ReviewsScreen.kt
+++ ui/src/main/java/com/cairosquad/ui/details/ReviewsScreen.kt
@@ -1,11 +1,15 @@
 package com.cairosquad.ui.details
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -40,24 +44,30 @@ private fun ReviewsContent(
     onBackClicked: () -> Unit,
     reviews: List<ReviewUiState>
 ) {
-    AppBar(
-        modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
-        title = stringResource(R.string.reviews),
-        onBackButtonClicked = onBackClicked,
-    )
-    LazyColumn(
-        modifier = Modifier.padding(top = 12.dp, start = 16.dp, end = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        items(reviews) { review ->
-            ReviewCard(
-                imgUrl = review.reviewerImageUrl,
-                rating = review.rating,
-                reviewDate = review.reviewDate,
-                reviewText = review.reviewText,
-                reviewerName = review.reviewerName
-            )
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .systemBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        AppBar(
+            modifier = Modifier.padding(vertical = 4.dp),
+            title = stringResource(R.string.reviews),
+            onBackButtonClicked = onBackClicked,
+        )
+        LazyColumn(
+            modifier = Modifier.padding(top = 12.dp, start = 16.dp, end = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(reviews) { review ->
+                ReviewCard(
+                    imgUrl = review.reviewerImageUrl,
+                    rating = review.rating,
+                    reviewDate = review.reviewDate,
+                    reviewText = review.reviewText,
+                    reviewerName = review.reviewerName
+                )
+            }
         }
     }
-
 }
