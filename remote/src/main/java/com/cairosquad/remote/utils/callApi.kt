d4f947d83dff7946fb8029a4d3f1c53ecd0cbe5b
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-07-17 15:56:05 +0300

    fix : rename common utils to utils in remote package

--- remote/src/main/java/com/cairosquad/remote/utils/callApi.kt
+++ remote/src/main/java/com/cairosquad/remote/utils/callApi.kt
@@ -1,4 +1,4 @@
-package com.cairosquad.remote.common.utils
+package com.cairosquad.remote.utils
 
 import com.cairosquad.repository.common.exception.NoInternetException
 import io.ktor.client.statement.HttpResponse
