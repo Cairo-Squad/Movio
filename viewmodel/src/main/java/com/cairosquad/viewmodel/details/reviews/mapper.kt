f60e696e0456fc1ab541890fd7f17af410116699
Menna Srour <mennasrour441@gmail.com>
2025-07-17 18:19:38 +0300

    feat:add function to format date and rating

--- viewmodel/src/main/java/com/cairosquad/viewmodel/details/reviews/mapper.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/details/reviews/mapper.kt
@@ -1,11 +1,29 @@
 package com.cairosquad.viewmodel.details.reviews
 
 import com.cairosquad.entity.Review
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 fun Review.toUiState() = ReviewsScreenState.ReviewUiState(
     reviewerName = author,
-    reviewDate = date.toString(),
-    rating = rating ,
-    reviewText = description ,
+    reviewDate = Timestamp(date).toDateFormat(),
+    rating = rating.toSingleDecimal(),
+    reviewText = description,
     reviewerImageUrl = authorPhotoPath
 )
+
+@JvmInline
+value class Timestamp(val value: Long)
+
+fun Timestamp.toDateFormat(): String {
+    val date = Date(this.value)
+    val format = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+    return format.format(date)
+}
+
+private fun String.toSingleDecimal(): String {
+    return this.toDoubleOrNull()
+        ?.let { String.format(Locale.getDefault(), "%.1f", it) }
+        ?: "0.0"
+}
