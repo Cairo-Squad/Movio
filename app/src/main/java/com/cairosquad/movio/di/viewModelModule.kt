0548ee43d30c57303601e2cc06847a0c7695fb3d
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-18 01:08:33 +0300

    add dependency of EpisodesViewModel

--- app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
@@ -1,9 +1,18 @@
 package com.cairosquad.movio.di
 
+import com.cairosquad.viewmodel.details.episodes.EpisodesDetailsViewModel
 import com.cairosquad.viewmodel.search.SearchViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
+    viewModel { (seriesId: Long, seasonNumber: Int) ->
+        EpisodesDetailsViewModel(
+            seriesDetailsUseCase = get(),
+            seriesId = seriesId,
+            seasonNumber = seasonNumber
+        )
+    }
 }
