e4dd129f4e1f89d019a1f2715b67362f73c94156
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-18 01:06:00 +0300

    change function ErrorHappened to ShowToast

--- viewmodel/src/main/java/com/cairosquad/viewmodel/details/episodes/EpisodesDetailEffect.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/details/episodes/EpisodesDetailEffect.kt
@@ -4,6 +4,6 @@ import com.cairosquad.viewmodel.exception.ErrorStatus
 
 sealed class EpisodesDetailEffect {
     data object NavigateBack : EpisodesDetailEffect()
-    data object PlayEpisodes : EpisodesDetailEffect()
-    data class ErrorHappened(val message: ErrorStatus) : EpisodesDetailEffect()
+    data object PlayEpisode : EpisodesDetailEffect()
+    data class ShowToast(val message: ErrorStatus) : EpisodesDetailEffect()
 }
