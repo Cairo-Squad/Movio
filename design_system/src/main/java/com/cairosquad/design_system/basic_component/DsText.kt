634f80d9596f55b9397a7f5bc4a2be198978457c
Menna Srour <mennasrour441@gmail.com>
2025-07-18 01:11:53 +0300

    refactor: wrap text and icon of matrial3 in design_system

---
+++ design_system/src/main/java/com/cairosquad/design_system/basic_component/DsText.kt
@@ -0,0 +1,28 @@
+package com.cairosquad.design_system.basic_component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import com.cairosquad.design_system.theme.Theme
+
+@Composable
+fun DsText(
+    text: String,
+    modifier: Modifier = Modifier,
+    style: TextStyle = Theme.textStyle.body.smallRegular10,
+    color: Color = Theme.color.surfaces.onSurface,
+    maxLines: Int = Int.MAX_VALUE,
+    overflow: TextOverflow = TextOverflow.Clip,
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        style = style,
+        color = color,
+        maxLines = maxLines,
+        overflow = overflow
+    )
+}
