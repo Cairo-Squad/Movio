dfc563b53e699a8c5ec5d18bb7653c162ec642ce
Zyad <zyadhammad531@gmail.com>
2025-07-18 00:41:23 +0300

    feature: set up for movie screen

--- ui/src/main/java/com/cairosquad/ui/details/MovieScreen.kt
+++ ui/src/main/java/com/cairosquad/ui/details/MovieScreen.kt
@@ -1,59 +1,45 @@
 package com.cairosquad.ui.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.cairosquad.design_system.theme.Theme
-import com.cairosquad.ui.movio_component.Chip
 import com.cairosquad.ui.navigation.LocalNavController
-import com.cairosquad.ui.navigation.ReviewsRoute
-import com.cairosquad.ui.navigation.SimilarMovieRoute
-import com.cairosquad.ui.navigation.TopCastRoute
+import com.cairosquad.viewmodel.details.movie.MovieInteractionListener
+import com.cairosquad.viewmodel.details.movie.MovieScreenState
+import com.cairosquad.viewmodel.details.movie.MovieViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun MovieScreen(
-    movieId: Long
+    movieId: Long,
+    viewModel: MovieViewModel = koinViewModel()
 ) {
     val navController = LocalNavController.current
 
-    Column(
+    val state by viewModel.screenState.collectAsState()
+
+
+    MovieContent(
+        state = state,
+        interactionListener = viewModel,
+    )
+}
+
+@Composable
+fun MovieContent(
+    state: MovieScreenState,
+    interactionListener: MovieInteractionListener,
+) {
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .windowInsetsPadding(WindowInsets.statusBars)
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterVertically)
+            .background(Theme.color.surfaces.surface)
     ) {
-        BasicText(
-            text = "Movie with id: $movieId",
-            style = Theme.textStyle.title.largeBold16
-                .copy(color = Theme.color.surfaces.onSurface),
-        )
-        Chip(
-            title = "back",
-            onClick = { navController.popBackStack() }
-        )
-        Chip(
-            title = "see all top cast for this movie",
-            onClick = { navController.navigate(TopCastRoute(movieId, true)) }
-        )
-        Chip(
-            title = "see reviews for this movie",
-            onClick = { navController.navigate(ReviewsRoute(movieId, true)) }
-        )
-        Chip(
-            title = "see similar movies for this movie",
-            onClick = { navController.navigate(SimilarMovieRoute(movieId)) }
-        )
-    }
 
+    }
 }
