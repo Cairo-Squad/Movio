72f212c39b5fd3583fb92284e5ebabbca1a5525d
youssef mohammed <youssef.mu.saber@gmail.com>
2025-07-19 02:00:26 +0300

    fixed merge conflict

--- app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
@@ -1,11 +1,10 @@
 package com.cairosquad.movio.di
 
-import com.cairosquad.viewmodel.details.similar_movies.SimilarMoviesViewModel
-import com.cairosquad.viewmodel.details.reviews.ReviewsViewModel
 import com.cairosquad.viewmodel.details.artist.ArtistViewModel
 import com.cairosquad.viewmodel.details.movie.MovieViewModel
 import com.cairosquad.viewmodel.details.reviews.ReviewsViewModel
 import com.cairosquad.viewmodel.details.series.SeriesDetailsViewModel
+import com.cairosquad.viewmodel.details.similar_movies.SimilarMoviesViewModel
 import com.cairosquad.viewmodel.details.top_cast.TopCastViewModel
 import com.cairosquad.viewmodel.search.SearchViewModel
 import org.koin.core.module.dsl.viewModel
