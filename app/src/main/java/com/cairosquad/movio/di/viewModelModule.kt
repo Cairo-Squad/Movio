6b7f7fc36588d88183176f245892aea32fd5fa3c
abdelrahman <abdelrahmankarim7@gmail.com>
2025-07-18 18:46:05 +0300

    feat : add similar movies viewmodel to di

--- app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
@@ -1,9 +1,11 @@
 package com.cairosquad.movio.di
 
+import com.cairosquad.viewmodel.details.similar_movies.SimilarMoviesViewModel
 import com.cairosquad.viewmodel.search.SearchViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
+    viewModelOf(::SimilarMoviesViewModel)
 }
