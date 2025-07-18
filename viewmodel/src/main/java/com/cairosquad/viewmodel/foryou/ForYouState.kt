2e6f3c9782cab8843db57d223a19ea36f6e8307c
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-19 01:53:14 +0300

    feat : create for you screen state

---
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/foryou/ForYouState.kt
@@ -0,0 +1,15 @@
+package com.cairosquad.viewmodel.foryou
+
+import androidx.paging.PagingData
+import com.cairosquad.viewmodel.exception.ErrorStatus
+import com.cairosquad.viewmodel.search.SearchScreenState.MovieUiState
+import com.cairosquad.viewmodel.search.SearchScreenState.ScreenStatus
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+data class ForYouState(
+    val forYou: Flow<PagingData<MovieUiState>> = flowOf(PagingData.empty()),
+    val screenStatus: ScreenStatus = ScreenStatus.LOADING,
+    val errorStatus: ErrorStatus? = null,
+    val isRefreshing: Boolean = false
+)
