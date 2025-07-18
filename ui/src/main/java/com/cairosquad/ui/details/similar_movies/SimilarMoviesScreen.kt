88c0a3ab42581c8c33dfa551021dfbae314d5a8e
abdelrahman <abdelrahmankarim7@gmail.com>
2025-07-18 18:48:55 +0300

    fix : remove unused imports

--- ui/src/main/java/com/cairosquad/ui/details/similar_movies/SimilarMoviesScreen.kt
+++ ui/src/main/java/com/cairosquad/ui/details/similar_movies/SimilarMoviesScreen.kt
@@ -1,14 +1,11 @@
 package com.cairosquad.ui.details.similar_movies
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -18,18 +15,18 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.cairosquad.design_system.R
 import com.cairosquad.design_system.basic_component.AppBar
-import com.cairosquad.viewmodel.details.similar_movies.SimilarMoviesViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.unit.dp
 import com.cairosquad.ui.movio_component.MovieCard
 import com.cairosquad.ui.navigation.MovieRoute
 import com.cairosquad.ui.utils.ObserveAsEffect
 import com.cairosquad.viewmodel.details.similar_movies.SimilarMoviesEffect
+import com.cairosquad.viewmodel.details.similar_movies.SimilarMoviesViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
