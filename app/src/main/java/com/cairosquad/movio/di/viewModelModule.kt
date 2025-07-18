c3b24e5565b424c6803b3fd30621a1ea32ce89c0
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-07-18 16:02:15 +0300

    fix: remove the fake viewmodel

--- app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
@@ -1,11 +1,9 @@
 package com.cairosquad.movio.di
 
-import com.cairosquad.viewmodel.ArtistsDetailsViewModel
 import com.cairosquad.viewmodel.search.SearchViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
-    viewModelOf(::ArtistsDetailsViewModel)
 }
