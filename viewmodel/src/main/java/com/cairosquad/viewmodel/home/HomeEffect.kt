e7a7fd8e42d9963cbd4f380c888e1dd6d6e8a524
gehadepada <gehadepada@gmail.com>
2025-07-20 15:08:11 +0300

    feat : implement HomeEffect

--- viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeEffect.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeEffect.kt
@@ -1,4 +1,16 @@
 package com.cairosquad.viewmodel.home
 
-class HomeEffect {
+sealed class HomeEffect {
+    data object NavigateToProfile: HomeEffect()
+    data class NavigateToCategory(val category:String): HomeEffect()
+    data class NavigateToFeaturedMovie(val movieID :Int) : HomeEffect()
+    data class NavigateMovie(val movieID :Int) : HomeEffect()
+    data class NavigateSeries(val movieID :Int) : HomeEffect()
+    data object NavigateToSeeAllTopRated: HomeEffect()
+    data object NavigateToSeeAllTrending: HomeEffect()
+    data object NavigateToSeeAllFreeToWatch: HomeEffect()
+    data object NavigateToSeeAllUpcoming: HomeEffect()
+    data object NavigateToSeeAllMoreRecommended: HomeEffect()
+    data object NavigateToSeeAllAiringToday: HomeEffect()
+    data object NavigateToSeeAllOnTv: HomeEffect()
 }
