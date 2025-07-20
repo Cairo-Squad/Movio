79369981f18c0f4e359abfa91cb74a1acce2972d
abdelrahman <abdelrahmankarim7@gmail.com>
2025-07-20 21:03:04 +0300

    feat: add new series retrieval methods in SeriesRepository and SeriesRepositoryImpl

--- domain/src/main/java/com/cairosquad/domain/repository/SeriesRepository.kt
+++ domain/src/main/java/com/cairosquad/domain/repository/SeriesRepository.kt
@@ -2,6 +2,7 @@ package com.cairosquad.domain.repository
 
 import com.cairosquad.entity.Artist
 import com.cairosquad.entity.Episode
+import com.cairosquad.entity.Movie
 import com.cairosquad.entity.Review
 import com.cairosquad.entity.Season
 import com.cairosquad.entity.Series
@@ -18,4 +19,22 @@ interface SeriesRepository {
     suspend fun getSimilarSeries(seriesId: Long, page: Int): List<Series>
 
     suspend fun getSeriesTopCast(seriesId: Long, page: Int): List<Artist>
+
+    suspend fun getTopRatingSeries(page: Int): List<Series>
+
+    suspend fun getMoreRecommendedSeries(page: Int): List<Series>
+
+    suspend fun getOnTvSeries(page: Int): List<Series>
+
+    suspend fun getAiringTodaySeries(page: Int): List<Series>
+
+    suspend fun getTrendingSeries(page: Int): List<Series>
+
+
+    suspend fun getFreeToWatchSeries(page: Int): List<Series>
+
+    suspend fun getSeriesByCategory(category: String, page: Int): List<Series>
+
+    suspend fun getRandomSeries(page: Int): List<Series>
+
 }
