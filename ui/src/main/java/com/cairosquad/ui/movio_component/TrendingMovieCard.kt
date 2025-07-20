bd5e1f721e4c73e15669fc5feaf1d114100ded67
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-20 11:07:42 +0300

    feat : create trending movie card component

---
+++ ui/src/main/java/com/cairosquad/ui/movio_component/TrendingMovieCard.kt
@@ -0,0 +1,42 @@
+package com.cairosquad.ui.movio_component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cairosquad.design_system.theme.MovioTheme
+import com.cairosquad.design_system.theme.Theme
+import com.cairosquad.ui.R
+
+@Composable
+fun TrendingMovieCard(imgUrl: String, modifier: Modifier = Modifier) {
+    Row(modifier = Modifier
+        .height(100.dp)
+        .fillMaxWidth()) {
+        Image(
+            modifier = Modifier
+                .size(height = 100.dp, width = 76.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(Theme.color.system.onError),
+            painter = painterResource(com.cairosquad.design_system.R.drawable.popcorn_ic),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TrendingMovieCardPrev(modifier: Modifier = Modifier) {
+    MovioTheme(isDarkTheme = true) {
+        TrendingMovieCard("")
+    }
+}
