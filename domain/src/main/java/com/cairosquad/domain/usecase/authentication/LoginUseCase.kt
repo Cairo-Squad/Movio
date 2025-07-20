6e2ca362360061ab154b1fc890ca0baca64efbe6
Zyad <zyadhammad531@gmail.com>
2025-07-20 17:51:54 +0300

    feature: finish login view model with out error handling

---
+++ domain/src/main/java/com/cairosquad/domain/usecase/authentication/LoginUseCase.kt
@@ -0,0 +1,14 @@
+package com.cairosquad.domain.usecase.authentication
+
+class LoginUseCase {
+
+    suspend fun login(username: String, password: String): String {
+        // TODO
+        return ""
+    }
+
+    suspend fun saveToken(token: String) {
+        // TODO
+        return
+    }
+}
--- viewmodel/src/main/java/com/cairosquad/viewmodel/login/LoginViewModel.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/login/LoginViewModel.kt
@@ -1,36 +1,110 @@
 package com.cairosquad.viewmodel.login
 
+import com.cairosquad.domain.usecase.authentication.LoginUseCase
 import com.cairosquad.viewmodel.base.BaseViewModel
 
 class LoginViewModel(
-
+    private val loginUseCase: LoginUseCase
 ) : BaseViewModel<LoginScreenState, LoginEffect>(LoginScreenState()), LoginInteractionListener {
     override fun onUsernameChange(username: String) {
-        TODO("Not yet implemented")
+        updateState {
+            it.copy(
+                username = username
+            )
+        }
     }
 
     override fun onPasswordChange(password: String) {
-        TODO("Not yet implemented")
+        updateState {
+            it.copy(
+                password = password
+            )
+        }
     }
 
     override fun onPasswordVisibilityIconClick() {
-        TODO("Not yet implemented")
+        updateState {
+            it.copy(
+                isPasswordVisible = !it.isPasswordVisible
+            )
+        }
     }
 
     override fun onForgetPasswordClick() {
-        TODO("Not yet implemented")
+        sendEffect(LoginEffect.NavigateToForgetPassword)
     }
 
     override fun onLoginClick() {
-        TODO("Not yet implemented")
+        tryToCall(
+            block = {
+                loginUseCase.login(
+                    username = screenState.value.username,
+                    password = screenState.value.password
+                )
+            },
+            onSuccess = {
+                saveToken(it)
+            },
+            onError = {
+                handleError(it)
+            },
+            onStart = {
+                updateState {
+                    it.copy(
+                        isLoading = true
+                    )
+                }
+            },
+            onEnd = {
+                updateState {
+                    it.copy(
+                        isLoading = false
+                    )
+                }
+            }
+        )
+    }
+
+    private fun saveToken(token: String) {
+        tryToCall(
+            block = {
+                loginUseCase.saveToken(token)
+            },
+            onSuccess = {
+                sendEffect(LoginEffect.NavigateToHome)
+            },
+            onError = {
+                handleError(it)
+            },
+            onStart = {
+                updateState {
+                    it.copy(
+                        isLoading = true
+                    )
+                }
+            },
+            onEnd = {
+                updateState {
+                    it.copy(
+                        isLoading = false
+                    )
+                }
+            }
+        )
     }
 
     override fun onContinueAsAGuestClick() {
-        TODO("Not yet implemented")
+        sendEffect(LoginEffect.NavigateToGuestHome)
     }
 
     override fun onSignUpClick() {
-        TODO("Not yet implemented")
+        sendEffect(LoginEffect.NavigateToSignUp)
     }
 
+
+    private fun handleError(
+        throwable: Throwable
+    ) {
+        // TODO
+    }
 }
