7a3f1b3e7ba4968990c9203d698d9e8e1385106b
mada <mohamedibra3232@gmail.com>
2025-07-20 18:21:25 +0300

    refactor: wrap all responses with call safe response

--- remote/src/main/java/com/cairosquad/remote/search/SearchApiService.kt
+++ remote/src/main/java/com/cairosquad/remote/search/SearchApiService.kt
@@ -4,6 +4,7 @@ import com.cairosquad.repository.search.data_source.remote.dto.ArtistRemoteDto
 import com.cairosquad.repository.search.data_source.remote.dto.MovieRemoteDto
 import com.cairosquad.repository.search.data_source.remote.dto.ResultResponse
 import com.cairosquad.repository.search.data_source.remote.dto.SeriesRemoteDto
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -13,25 +14,25 @@ interface SearchApiService {
     suspend fun getMovies(
         @Query("query") query: String,
         @Query("page") page: Int
-    ): ResultResponse<MovieRemoteDto>
+    ): Response<ResultResponse<MovieRemoteDto>>
 
     @GET("search/tv")
     suspend fun getSeries(
         @Query("query") query: String,
         @Query("page") page: Int
-    ): ResultResponse<SeriesRemoteDto>
+    ): Response<ResultResponse<SeriesRemoteDto>>
 
     @GET("search/person")
     suspend fun getArtists(
         @Query("query") query: String,
         @Query("page") page: Int
-    ): ResultResponse<ArtistRemoteDto>
+    ): Response<ResultResponse<ArtistRemoteDto>>
 
     @GET("movie/top_rated")
     suspend fun getPersonalizedMovies(
         @Query("page") page: Int
-    ): ResultResponse<MovieRemoteDto>
+    ): Response<ResultResponse<MovieRemoteDto>>
 
     @GET("movie/now_playing")
-    suspend fun getSuggestedMovies(): ResultResponse<MovieRemoteDto>
+    suspend fun getSuggestedMovies(): Response<ResultResponse<MovieRemoteDto>>
 }
--- remote/src/main/java/com/cairosquad/remote/series/SeriesApiService.kt
+++ remote/src/main/java/com/cairosquad/remote/series/SeriesApiService.kt
@@ -7,44 +7,45 @@ import com.cairosquad.repository.search.data_source.remote.dto.SeriesRemoteDto
 import com.cairosquad.repository.series.data_source.remote.dto.SeasonResponse
 import com.cairosquad.repository.series.data_source.remote.dto.SeriesDetailsRemoteDto
 import com.cairosquad.repository.series.data_source.remote.dto.SeriesResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SeriesApiService {
 
-    @GET
+    @GET("tv/{seriesId}")
     suspend fun getSeries(
         @Path("seriesId") seriesId: Long
-    ): SeriesDetailsRemoteDto
+    ): Response<SeriesDetailsRemoteDto>
 
     @GET("tv/{seriesId}/reviews")
     suspend fun getSeriesReviews(
         @Path("seriesId") seriesId: Long,
         @Query("page") page: Int
-    ): ResultResponse<ReviewRemoteDto>
+    ): Response<ResultResponse<ReviewRemoteDto>>
 
     @GET("tv/{seriesId}/similar")
     suspend fun getSimilarSeries(
         @Path("seriesId") seriesId: Long,
         @Query("page") page: Int
-    ): ResultResponse<SeriesRemoteDto>
+    ): Response<ResultResponse<SeriesRemoteDto>>
 
     @GET("tv/{seriesId}/credits")
     suspend fun getSeriesTopCast(
         @Path("seriesId") seriesId: Long,
         @Query("page") page: Int
-    ): CreditResponse
+    ): Response<CreditResponse>
 
     @GET("tv/{seriesId}")
     suspend fun getSeriesSeasons(
         @Path("seriesId") seriesId: Long
-    ): SeriesResponse
+    ): Response<SeriesResponse>
 
     @GET("tv/{seriesId}/season/{seasonNumber}")
     suspend fun getEpisodes(
         @Path("seriesId") seriesId: Long,
         @Path("seasonNumber") seasonNumber: Int
-    ): SeasonResponse
+    ): Response<SeasonResponse>
 
 }
