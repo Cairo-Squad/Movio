4a7f6a23ad41d126d23d5c474438f49d381bc668
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-20 15:49:15 +0300

    build the retrofit builder

---
+++ remote/src/main/java/com/cairosquad/remote/utils/retrofit/provideRetrofit.kt
@@ -0,0 +1,38 @@
+package com.cairosquad.remote.utils.retrofit
+
+import com.cairosquad.remote.BuildConfig
+import com.cairosquad.remote.utils.retrofit.interceptor.ApiKeyInterceptor
+import com.cairosquad.remote.utils.retrofit.interceptor.AuthInterceptor
+import com.cairosquad.remote.utils.retrofit.interceptor.LanguageInterceptor
+import kotlinx.serialization.json.Json
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+fun provideRetrofit(tokenProvider: () -> String?): Retrofit {
+    val contentType = "application/json".toMediaType()
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(ApiKeyInterceptor(BuildConfig.API_KEY))
+        .addInterceptor(AuthInterceptor(tokenProvider))
+        .addInterceptor(LanguageInterceptor())
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    val json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+        isLenient = true
+    }
+
+    return Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(client)
+        .addConverterFactory(json.asConverterFactory(contentType))
+        .build()
+}
+
