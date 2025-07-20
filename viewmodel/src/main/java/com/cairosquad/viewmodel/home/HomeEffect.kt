5505cffae5b209e27e4a2c65e1d5f413779d1560
gehadepada <gehadepada@gmail.com>
2025-07-20 16:08:40 +0300

    feat : put structure for HomeViewModel

--- viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeEffect.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeEffect.kt
@@ -3,9 +3,9 @@ package com.cairosquad.viewmodel.home
 sealed class HomeEffect {
     data object NavigateToProfile: HomeEffect()
     data class NavigateToCategory(val category:String): HomeEffect()
-    data class NavigateToFeaturedMovie(val movieID :Int) : HomeEffect()
-    data class NavigateMovie(val movieID :Int) : HomeEffect()
-    data class NavigateSeries(val movieID :Int) : HomeEffect()
+    data class NavigateToFeaturedMovie(val movieID :Long) : HomeEffect()
+    data class NavigateMovie(val movieID :Long) : HomeEffect()
+    data class NavigateSeries(val movieID :Long) : HomeEffect()
     data object NavigateToSeeAllTopRated: HomeEffect()
     data object NavigateToSeeAllTrending: HomeEffect()
     data object NavigateToSeeAllFreeToWatch: HomeEffect()
--- viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeViewModel.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeViewModel.kt
@@ -1,4 +1,57 @@
 package com.cairosquad.viewmodel.home
 
-class HomeViewModel {
+import com.cairosquad.viewmodel.base.BaseViewModel
+
+class HomeViewModel(
+
+
+)  : BaseViewModel<HomeScreenState,HomeEffect>(HomeScreenState()),
+    HomeInteractionsListener{
+    override fun onClickProfile() {
+        sendEffect(HomeEffect.NavigateToProfile)
+    }
+
+    override fun onClickCategory(category: String) {
+        sendEffect(HomeEffect.NavigateToCategory(category))
+    }
+
+    override fun onClickFeaturedMovie(movieId: Long) {
+        sendEffect(HomeEffect.NavigateToFeaturedMovie(movieId))
+    }
+
+    override fun onClickMovie(movieId: Long) {
+        sendEffect(HomeEffect.NavigateMovie(movieId))
+    }
+
+    override fun onClickSeries(seriesId: Long) {
+        sendEffect(HomeEffect.NavigateSeries(seriesId))
+    }
+
+    override fun onClickSeeAllTopRated() {
+        sendEffect(HomeEffect.NavigateToSeeAllTopRated)
+    }
+
+    override fun onClickSeeAllTrending() {
+        sendEffect(HomeEffect.NavigateToSeeAllTrending)
+    }
+
+    override fun onClickSeeAllFreeToWatch() {
+        sendEffect(HomeEffect.NavigateToSeeAllFreeToWatch)
+    }
+
+    override fun onClickSeeAllUpcoming() {
+        sendEffect(HomeEffect.NavigateToSeeAllUpcoming)
+    }
+
+    override fun onClickSeeAllMoreRecommended() {
+        sendEffect(HomeEffect.NavigateToSeeAllMoreRecommended)
+    }
+
+    override fun onClickSeeAllAiringToday() {
+        sendEffect(HomeEffect.NavigateToSeeAllAiringToday)
+    }
+
+    override fun onClickSeeAllOnTv() {
+        sendEffect(HomeEffect.NavigateToSeeAllOnTv)
+    }
 }
