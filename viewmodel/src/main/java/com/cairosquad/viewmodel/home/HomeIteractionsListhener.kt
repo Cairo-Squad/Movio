3aed1b814371fe0fa37b15f9fe81d93b77c368a4
gehadepada <gehadepada@gmail.com>
2025-07-20 14:49:21 +0300

    feat :make HomeUserInteractions

--- viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeIteractionsListhener.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeIteractionsListhener.kt
@@ -1,4 +1,16 @@
 package com.cairosquad.viewmodel.home
 
 interface HomeIteractionsListhener {
+    fun onClickProfile()
+    fun onClickCategory(category: String)
+    fun onClickFeaturedMovie(movieId: Long)
+    fun onClickMovie(movieId: Long)
+    fun onClickSeries(seriesId: Long)
+    fun onClickSeeAllTopRated()
+    fun onClickSeeAllTrending()
+    fun onClickSeeAllFreeToWatch()
+    fun onClickSeeAllUpcoming()
+    fun onClickSeeAllMoreRecommended()
+    fun onClickSeeAllAiringToday()
+    fun onClickSeeAllOnTv()
 }
