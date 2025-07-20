597dc49ab5972ab7177051295ec4f3fd6bae42b3
gehadepada <gehadepada@gmail.com>
2025-07-20 14:21:13 +0300

    feat :make Home Screen State

--- viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeScreenState.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/home/HomeScreenState.kt
@@ -1,4 +1,34 @@
 package com.cairosquad.viewmodel.home
 
-class HomeScreenState {
+import com.cairosquad.entity.Genre
+import com.cairosquad.viewmodel.details.artist.ArtistScreenState
+import com.cairosquad.viewmodel.exception.ErrorStatus
+
+class HomeScreenState (
+    val Movies :List<ArtistScreenState.MovieUiState> = emptyList(),
+    val Series :List<ArtistScreenState.SeriesUiState> =emptyList(),
+    val screenStatus: ArtistScreenState.ScreenStatus = ArtistScreenState.ScreenStatus.LOADING,
+    val errorStatus: ErrorStatus? = null,
+) {
+    data class MovieUiState(
+        val id: Long = 0L,
+        val title: String = "",
+        val rating: Float = 0f,
+        val posterPath: String = "",
+        val genres: List<Genre> =emptyList()
+    )
+
+    data class SeriesUiState(
+        val id: Long = 0L,
+        val title: String = "",
+        val rating: Float = 0f,
+        val posterPath: String = "",
+        val genres: List<Genre> = emptyList()
+    )
+
+    enum class ScreenStatus {
+        LOADING,
+        SUCCESS,
+        FAILED
+    }
 }
