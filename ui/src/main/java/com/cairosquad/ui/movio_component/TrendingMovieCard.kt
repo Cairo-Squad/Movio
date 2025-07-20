8b97795f1c61310cb8ce869278bc93c4e42e2b0c
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-20 11:24:54 +0300

    feat : create trending movie card component

--- ui/src/main/java/com/cairosquad/ui/movio_component/TrendingMovieCard.kt
+++ ui/src/main/java/com/cairosquad/ui/movio_component/TrendingMovieCard.kt
@@ -2,9 +2,11 @@ package com.cairosquad.ui.movio_component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -19,9 +21,11 @@ import com.cairosquad.ui.R
 
 @Composable
 fun TrendingMovieCard(imgUrl: String, modifier: Modifier = Modifier) {
-    Row(modifier = Modifier
-        .height(100.dp)
-        .fillMaxWidth()) {
+    Row(
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+    ) {
         Image(
             modifier = Modifier
                 .size(height = 100.dp, width = 76.dp)
@@ -30,6 +34,9 @@ fun TrendingMovieCard(imgUrl: String, modifier: Modifier = Modifier) {
             painter = painterResource(com.cairosquad.design_system.R.drawable.popcorn_ic),
             contentDescription = null
         )
+        Column(modifier = Modifier.padding(start = 8.dp)) {
+
+        }
     }
 }
 
