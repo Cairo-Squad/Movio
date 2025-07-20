1955e49c4be04896badd7e46e7e13edb274e0848
mada <mohamedibra3232@gmail.com>
2025-07-20 14:05:28 +0300

    feat: created a search api service to migrate from Ktor to Retrofit.

---
+++ remote/src/main/java/com/cairosquad/remote/series/SeriesApiService.kt
@@ -0,0 +1,49 @@
+package com.cairosquad.remote.series
+
+import com.cairosquad.repository.movie.data_source.remote.dto.CreditResponse
+import com.cairosquad.repository.movie.data_source.remote.dto.ReviewRemoteDto
+import com.cairosquad.repository.search.data_source.remote.dto.ResultResponse
+import com.cairosquad.repository.search.data_source.remote.dto.SeriesRemoteDto
+import com.cairosquad.repository.series.data_source.remote.dto.SeasonResponse
+import com.cairosquad.repository.series.data_source.remote.dto.SeriesDetailsRemoteDto
+import com.cairosquad.repository.series.data_source.remote.dto.SeriesResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface SeriesApiService {
+
+    @GET
+    suspend fun getSeries(
+        @Path("seriesId") seriesId: Long
+    ): SeriesDetailsRemoteDto
+
+    @GET("tv/{seriesId}/reviews")
+    suspend fun getSeriesReviews(
+        @Path("seriesId") seriesId: Long,
+        @Query("page") page: Int
+    ): ResultResponse<ReviewRemoteDto>
+
+    @GET("tv/{seriesId}/similar")
+    suspend fun getSimilarSeries(
+        @Path("seriesId") seriesId: Long,
+        @Query("page") page: Int
+    ): ResultResponse<SeriesRemoteDto>
+
+    @GET("tv/{seriesId}/credits")
+    suspend fun getSeriesTopCast(
+        @Path("seriesId") seriesId: Long,
+        @Query("page") page: Int
+    ): CreditResponse
+
+    @GET("tv/{seriesId}")
+    suspend fun getSeriesSeasons(
+        @Path("seriesId") seriesId: Long
+    ): SeriesResponse
+
+    @GET("tv/{seriesId}/season/{seasonNumber}")
+    suspend fun getEpisodes(
+        @Path("seriesId") seriesId: Long,
+        @Path("seasonNumber") seasonNumber: Int
+    ): SeasonResponse
+}
