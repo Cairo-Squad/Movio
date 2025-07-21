1e5ab5b5797526288e8c3e1a2dada64397715ab1
Zyad <zyadhammad531@gmail.com>
2025-07-21 16:09:38 +0300

    feature: add dtos and data source abstractions

---
+++ repository/src/main/java/com/cairosquad/repository/login/data_source/remote/LoginDataSource.kt
@@ -0,0 +1,16 @@
+package com.cairosquad.repository.login.data_source.remote
+
+import com.cairosquad.repository.login.data_source.remote.dto.RequestTokenResponse
+import com.cairosquad.repository.login.data_source.remote.dto.SessionIdResponse
+
+interface LoginDataSource {
+    suspend fun createRequestToken(): RequestTokenResponse
+
+    suspend fun authenticateRequestToken(
+        username: String,
+        password: String,
+        requestToken: String
+    ): RequestTokenResponse
+
+    suspend fun createSessionId(requestToken: String): SessionIdResponse
+}
