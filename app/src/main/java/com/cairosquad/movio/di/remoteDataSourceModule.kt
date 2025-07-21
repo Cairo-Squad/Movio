815948ff5a6077dc74d2ce5980bbf7b07280c92d
Zyad <zyadhammad531@gmail.com>
2025-07-21 16:42:47 +0300

    feature: add remote data source and service for login

--- app/src/main/java/com/cairosquad/movio/di/remoteDataSourceModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/remoteDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.cairosquad.movio.di
 
 import com.cairosquad.remote.artists.ArtistsApiService
 import com.cairosquad.remote.artists.RemoteArtistDataSourceImpl
+import com.cairosquad.remote.login.LoginApiService
+import com.cairosquad.remote.login.LoginDataSourceImpl
 import com.cairosquad.remote.movie.MovieApiService
 import com.cairosquad.remote.movie.RemoteMovieDataSourceImpl
 import com.cairosquad.remote.search.RemoteMovieDiscoveryDataSourceImpl
@@ -11,6 +13,7 @@ import com.cairosquad.remote.series.RemoteSeriesDataSourceImpl
 import com.cairosquad.remote.series.SeriesApiService
 import com.cairosquad.remote.utils.retrofit.retrofitProvider
 import com.cairosquad.repository.artists.data_source.ArtistsRemoteDataSource
+import com.cairosquad.repository.login.data_source.remote.LoginDataSource
 import com.cairosquad.repository.movie.data_source.remote.RemoteMovieDataSource
 import com.cairosquad.repository.search.data_source.remote.RemoteMovieDiscoveryDataSource
 import com.cairosquad.repository.search.data_source.remote.RemoteSearchDataSource
@@ -26,12 +29,20 @@ val remoteDataSourceModule = module {
         )
     }
 
+    single<LoginApiService> {
+        get<Retrofit>().create(LoginApiService::class.java)
+    }
+
     single<SeriesApiService> {
         get<Retrofit>().create(SeriesApiService::class.java)
     }
 
     single<SearchApiService> {
         get<Retrofit>().create(SearchApiService::class.java)
+    }
+
+    single<LoginDataSource> {
+        LoginDataSourceImpl(get())
     }
 
     single<RemoteSearchDataSource> {
