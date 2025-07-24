c6b7db5bf9758cfb9e49c17f52f9d8a54752d381
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-07-24 20:15:21 +0300

    make view model consistent

--- viewmodel/src/main/java/com/cairosquad/viewmodel/foryou/ForYouViewModel.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/foryou/ForYouViewModel.kt
@@ -6,6 +6,7 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import com.cairosquad.domain.exception.MovioException
 import com.cairosquad.viewmodel.base.BaseViewModel
+import com.cairosquad.viewmodel.details.artist.ArtistScreenState
 import com.cairosquad.viewmodel.exception.ErrorStatus
 import com.cairosquad.viewmodel.exception.exceptionToErrorStatus
 import com.cairosquad.viewmodel.foryou.ForYouState.ScreenStatus
