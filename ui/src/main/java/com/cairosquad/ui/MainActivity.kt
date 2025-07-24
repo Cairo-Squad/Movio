6b9f2be944a41e1803f62f731484e7cb1610a6dc
NadeenXo <nadeenserag@gmail.com>
2025-07-24 13:30:05 +0300

    feature: edit in logo to match design

--- ui/src/main/java/com/cairosquad/ui/MainActivity.kt
+++ ui/src/main/java/com/cairosquad/ui/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.cairosquad.ui
 
+import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.cairosquad.design_system.theme.MovioTheme
 import com.cairosquad.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        installSplashScreen().setOnExitAnimationListener { splashScreenView ->
+            ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.ALPHA,
+                1f,
+                0f
+            ).apply {
+                interpolator = DecelerateInterpolator()
+                duration = 250
+                doOnEnd { splashScreenView.remove() }
+                start()
+            }
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
