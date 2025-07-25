079f96a8ef077f8519c69c12a924cf354f7527a2
Menna Srour <111497966+MennaSrour@users.noreply.github.com>
2025-07-25 18:47:10 +0300

    remove the repeated junit dependency from view model gradle

--- viewmodel/build.gradle.kts
+++ viewmodel/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     testImplementation(libs.junit)
-    testImplementation(libs.junit.junit)
-    testImplementation(libs.junit.junit)
     testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     implementation(libs.androidx.lifecycle.runtime.ktx)
