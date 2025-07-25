015dd903a5c97261df97f86a028014634680443a
Menna Srour <111497966+MennaSrour@users.noreply.github.com>
2025-07-25 18:59:12 +0300

    add un important space to test ci problem

--- app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
+++ app/src/main/java/com/cairosquad/movio/di/viewModelModule.kt
@@ -20,6 +20,7 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
+
 val viewModelModule = module {
     viewModelOf(::SearchViewModel)
     viewModelOf(::ForYouViewModel)
