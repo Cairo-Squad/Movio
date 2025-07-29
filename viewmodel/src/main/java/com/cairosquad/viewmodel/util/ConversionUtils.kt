aba4746390e60362ca0ff51014618a8778b1ec70
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-07-29 17:32:01 +0300

    fix: crash for arabic rating

--- viewmodel/src/main/java/com/cairosquad/viewmodel/util/ConversionUtils.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/util/ConversionUtils.kt
@@ -1,5 +1,9 @@
 package com.cairosquad.viewmodel.util
 
 fun Float.roundToFirstDecimalPlace(): Float {
-    return "%.1f".format(this).toFloat()
+    return try {
+        "%.1f".format(this).toFloat()
+    } catch (_: Exception) {
+        this.times(10).toInt().times(0.1f)
+    }
 }
