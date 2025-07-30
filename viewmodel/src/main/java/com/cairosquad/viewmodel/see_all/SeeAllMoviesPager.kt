c8f54352cf6996f63f51b6095bc2529015c17651
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-30 13:24:51 +0300

    feat : create see all movies pager

---
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/see_all/SeeAllMoviesPager.kt
@@ -0,0 +1,51 @@
+package com.cairosquad.viewmodel.see_all
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.cairosquad.domain.usecase.ManageMoviesUseCase
+import com.cairosquad.entity.Movie
+import kotlinx.coroutines.flow.Flow
+
+class SeeAllMoviesPager(
+    private val manageMoviesUseCase: ManageMoviesUseCase
+) {
+    fun getTopRatingMovies(genreId: Long?): Flow<PagingData<Movie>> =
+        createPager(genreId) { page, genre ->
+            manageMoviesUseCase.getTopRatingMovies(page, genre)
+        }
+
+    fun getTrendingMovies(genreId: Long?): Flow<PagingData<Movie>> =
+        createPager(genreId) { page, genre -> manageMoviesUseCase.getTrendingMovies(page, genre) }
+
+    fun getFreeToWatchMovies(genreId: Long?): Flow<PagingData<Movie>> =
+        createPager(genreId) { page, genre ->
+            manageMoviesUseCase.getFreeToWatchMovies(
+                page,
+                genre
+            )
+        }
+
+    fun getUpcomingMovies(genreId: Long?): Flow<PagingData<Movie>> =
+        createPager(genreId) { page, genre -> manageMoviesUseCase.getUpcomingMovies(page, genre) }
+
+    fun getMoreRecommendedMovies(genreId: Long?): Flow<PagingData<Movie>> =
+        createPager(genreId) { page, genre ->
+            manageMoviesUseCase.getMoreRecommendedMovies(
+                page,
+                genre
+            )
+        }
+
+    private fun <T : Any> createPager(
+        genreId: Long? = null,
+        fetcher: suspend (Int, Long?) -> List<T>
+    ): Flow<PagingData<T>> {
+        return Pager(
+            config = PagingConfig(pageSize = 20, prefetchDistance = 6),
+            pagingSourceFactory = {
+                SeeAllPagingSource { page, _ -> fetcher(page, genreId) }
+            }
+        ).flow
+    }
+}
