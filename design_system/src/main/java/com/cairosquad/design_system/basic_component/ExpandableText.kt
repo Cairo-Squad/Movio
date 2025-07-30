e3edea2a6c914e23fb2cdb1c70c03e8b86762e74
Menna Srour <111497966+MennaSrour@users.noreply.github.com>
2025-07-30 18:06:05 +0300

    Update ExpandableText.kt

--- design_system/src/main/java/com/cairosquad/design_system/basic_component/ExpandableText.kt
+++ design_system/src/main/java/com/cairosquad/design_system/basic_component/ExpandableText.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberSaveable
+androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
