d706a914ef1dc55f03382b28d88442fac40ecf60
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-30 13:33:01 +0300

    refactor : edit see all screen and view model

--- ui/src/main/java/com/cairosquad/ui/see_all_screen/SeeAllScreen.kt
+++ ui/src/main/java/com/cairosquad/ui/see_all_screen/SeeAllScreen.kt
@@ -1,8 +1,5 @@
 package com.cairosquad.ui.see_all_screen
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -19,8 +16,6 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,8 +28,11 @@ import androidx.compose.ui.draw.blur
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.cairosquad.design_system.R
 import com.cairosquad.design_system.basic_component.AppBar
+import com.cairosquad.design_system.basic_component.RefreshBox
 import com.cairosquad.design_system.theme.Theme
 import com.cairosquad.ui.movio_component.CategoriesChips
 import com.cairosquad.ui.movio_component.MovieCard
@@ -62,12 +60,28 @@ fun SeeAllScreen(
     val state by viewModel.screenState.collectAsState()
 
     val navController = LocalNavController.current
-
+    val media = state.mediaList.collectAsLazyPagingItems()
     LaunchedEffect(contentType, mediaType) {
         viewModel.loadData(
-            contentType = contentType,
-            mediaType = mediaType
+            contentType = contentType, mediaType = mediaType
         )
+    }
+    LaunchedEffect(media.loadState) {
+        when (val state = media.loadState.refresh) {
+            is androidx.paging.LoadState.Loading -> {
+                viewModel.updateScreenStatus(SeeAllScreenState.ScreenStatus.LOADING)
+            }
+
+            is androidx.paging.LoadState.NotLoading -> {
+                viewModel.updateScreenStatus(SeeAllScreenState.ScreenStatus.SUCCESS)
+            }
+
+            is androidx.paging.LoadState.Error -> {
+                viewModel.updateScreenStatus(SeeAllScreenState.ScreenStatus.FAILED)
+                val errorStatus = viewModel.handleSearchException(state.error)
+                viewModel.updateErrorStatus(errorStatus)
+            }
+        }
     }
     ObserveAsEffect(viewModel.effect) { effect ->
         effectDiscoverHandler(
@@ -75,117 +89,103 @@ fun SeeAllScreen(
         )
     }
 
-    Box(modifier = Modifier.background(Theme.color.surfaces.surface)) {
-        Box(
-            modifier = Modifier
-                .windowInsetsPadding(WindowInsets.statusBars)
-                .align(Alignment.TopEnd)
-                .size(230.dp)
-                .blur(263.85.dp, edgeTreatment = BlurredEdgeTreatment.Unbounded)
-                .background(Theme.color.surfaces.onSurfaceAt5, shape = CircleShape)
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .windowInsetsPadding(WindowInsets.navigationBars)
-        ) {
-            AppBar(
-                modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars),
-                title = stringResource(contentType.titleId),
-                onBackButtonClicked = {
-                    navController.popBackStack()
-                },
-                onShareButtonClicked = null,
-                onFavoriteButtonClicked = null
+    RefreshBox(
+        isRefreshing = state.isRefreshing,
+        onRefresh = { viewModel.onRefresh(state.selectedGenreIndex) },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(modifier = Modifier.background(Theme.color.surfaces.surface)) {
+            Box(
+                modifier = Modifier
+                    .windowInsetsPadding(WindowInsets.statusBars)
+                    .align(Alignment.TopEnd)
+                    .size(230.dp)
+                    .blur(263.85.dp, edgeTreatment = BlurredEdgeTreatment.Unbounded)
+                    .background(Theme.color.surfaces.onSurfaceAt5, shape = CircleShape)
             )
-
-            CategoriesChips(
-                modifier = Modifier.padding(top = 16.dp),
-                categories = state.genres.map { it.name },
-                selectedChipIndex = state.selectedGenreIndex,
-                onChipSelected = { index ->
-                    viewModel.onGenreSelected(index)
-                })
-
-            if (contentType != MediaContentType.TRENDING) {
-                SeeAllMediaItems(
-                    modifier = Modifier
-                        .padding(top = 24.dp, bottom = 16.dp)
-                        .padding(horizontal = 16.dp),
-                    state = state,
-                    listener = viewModel
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .windowInsetsPadding(WindowInsets.navigationBars)
+            ) {
+                AppBar(
+                    modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars),
+                    title = stringResource(contentType.titleId),
+                    onBackButtonClicked = {
+                        navController.popBackStack()
+                    },
+                    onShareButtonClicked = null,
+                    onFavoriteButtonClicked = null
                 )
-            } else {
-                TrendingContentList(
-                    modifier = Modifier
-                        .padding(top = 24.dp, bottom = 16.dp)
-                        .padding(horizontal = 16.dp),
-                    listener = viewModel,
-                    state = state,
-                )
+
+                CategoriesChips(
+                    modifier = Modifier.padding(top = 16.dp),
+                    categories = state.genres.map { it.name },
+                    selectedChipIndex = state.selectedGenreIndex,
+                    onChipSelected = { index ->
+                        viewModel.onGenreSelected(index)
+                    })
+
+                if (contentType != MediaContentType.TRENDING) {
+                    SeeAllMediaItems(
+                        modifier = Modifier
+                            .padding(top = 24.dp, bottom = 16.dp)
+                            .padding(horizontal = 16.dp),
+                        state = state,
+                        listener = viewModel,
+                        media = media
+                    )
+                } else {
+                    TrendingContentList(
+                        modifier = Modifier
+                            .padding(top = 24.dp, bottom = 16.dp)
+                            .padding(horizontal = 16.dp),
+                        listener = viewModel,
+                        state = state,
+                        media = media
+                    )
+                }
             }
         }
     }
 }
+
 @Composable
 private fun SeeAllMediaItems(
+    media: LazyPagingItems<SeeAllScreenState.MediaUiState>,
     state: SeeAllScreenState,
     listener: SeeAllInteractionsListener,
     modifier: Modifier = Modifier
 ) {
+
     when {
 
         state.screenStatus == SeeAllScreenState.ScreenStatus.FAILED -> {
-            AnimatedVisibility(visible = true, enter = fadeIn(), exit = fadeOut()) {
-                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    StateMessage(
-                        imageDrawable = R.drawable.no_internet,
-                        titleId = R.string.no_internet_connection,
-                        descriptionId = R.string.internet_is_not_available_description
-                    )
-                }
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                StateMessage(
+                    imageDrawable = R.drawable.no_internet,
+                    titleId = R.string.no_internet_connection,
+                    descriptionId = R.string.internet_is_not_available_description
+                )
             }
         }
 
-        state.isLoading -> {
+        state.screenStatus == SeeAllScreenState.ScreenStatus.LOADING -> {
             SeeAllLoadingScreen(modifier)
         }
 
-        !state.isLoading && state.mediaList.isEmpty() -> {
-            AnimatedVisibility(visible = true, enter = fadeIn(), exit = fadeOut()) {
-                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    StateMessage(
-                        imageDrawable = R.drawable.no_result,
-                        titleId = R.string.no_results_found,
-                        descriptionId = R.string.no_results_found_description
-                    )
-                }
+        state.isEmpty -> {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                StateMessage(
+                    imageDrawable = R.drawable.no_result,
+                    titleId = R.string.no_results_found,
+                    descriptionId = R.string.no_results_found_description
+                )
             }
         }
 
         else -> {
-            AnimatedVisibility(visible = true, enter = fadeIn(), exit = fadeOut()) {
-                LazyVerticalGrid(
-                    modifier = modifier.fillMaxSize(),
-                    columns = GridCells.Adaptive(minSize = 101.33.dp),
-                    horizontalArrangement = Arrangement.spacedBy(12.dp),
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
-                    contentPadding = PaddingValues(bottom = 16.dp)
-                ) {
-                    items(state.mediaList) { item ->
-                        MovieCard(
-                            modifier = Modifier.clickable {
-                                listener.onClickMedia(item.id, item.isMovie)
-                            },
-                            title = item.title,
-                            vote = item.rating,
-                            imgUrl = item.posterPath,
-                            width = null,
-                            aspectRatio = 0.743f
-                        )
-                    }
-                }
-            }
+            MediaGridContent(media, listener, modifier)
         }
     }
 }
@@ -193,61 +193,96 @@ private fun SeeAllMediaItems(
 @Composable
 private fun TrendingContentList(
     listener: SeeAllInteractionsListener,
+    media: LazyPagingItems<SeeAllScreenState.MediaUiState>,
     state: SeeAllScreenState,
     modifier: Modifier = Modifier
 ) {
     when {
 
         state.screenStatus == SeeAllScreenState.ScreenStatus.FAILED -> {
-            AnimatedVisibility(visible = true, enter = fadeIn(), exit = fadeOut()) {
-                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    StateMessage(
-                        imageDrawable = R.drawable.no_internet,
-                        titleId = R.string.no_internet_connection,
-                        descriptionId = R.string.internet_is_not_available_description
-                    )
-                }
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                StateMessage(
+                    imageDrawable = R.drawable.no_internet,
+                    titleId = R.string.no_internet_connection,
+                    descriptionId = R.string.internet_is_not_available_description
+                )
             }
         }
 
-        state.isLoading -> {
+        state.screenStatus == SeeAllScreenState.ScreenStatus.LOADING -> {
             SeeAllLoadingScreen(modifier)
         }
 
-        !state.isLoading && state.mediaList.isEmpty() -> {
-            AnimatedVisibility(visible = true, enter = fadeIn(), exit = fadeOut()) {
-                Box(
-                    modifier = modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    StateMessage(
-                        imageDrawable = R.drawable.no_result,
-                        titleId = R.string.no_results_found,
-                        descriptionId = R.string.no_results_found_description
-                    )
-                }
+        state.isEmpty -> {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                StateMessage(
+                    imageDrawable = R.drawable.no_result,
+                    titleId = R.string.no_results_found,
+                    descriptionId = R.string.no_results_found_description
+                )
             }
         }
 
         else -> {
-            AnimatedVisibility(visible = true, enter = fadeIn(), exit = fadeOut()) {
-                LazyColumn(
-                    modifier = modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
-                    contentPadding = PaddingValues(bottom = 16.dp)
-                ) {
-                    items(state.mediaList) { mediaItem ->
-                        TrendingMovieCard(
-                            modifier = Modifier.clickable {
-                                listener.onClickMedia(mediaItem.id, mediaItem.isMovie)
-                            },
-                            imgUrl = mediaItem.posterPath,
-                            rating = String.format(Locale.getDefault(), "%.1f", mediaItem.rating),
-                            movieTitle = mediaItem.title,
-                            movieCategory = mediaItem.genres.firstOrNull()?.name.orEmpty()
-                        )
-                    }
-                }
+            MediaListContent(media, listener, modifier)
+        }
+    }
+}
+
+@Composable
+fun MediaGridContent(
+    media: LazyPagingItems<SeeAllScreenState.MediaUiState>,
+    listener: SeeAllInteractionsListener,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        modifier = modifier.fillMaxSize(),
+        columns = GridCells.Adaptive(minSize = 101.33.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(bottom = 16.dp)
+    ) {
+        items(media.itemCount) { index ->
+            media[index]?.let { item ->
+                MovieCard(
+                    modifier = Modifier.clickable {
+                        listener.onClickMedia(item.id, item.isMovie)
+                    },
+                    title = item.title,
+                    vote = item.rating,
+                    imgUrl = item.posterPath,
+                    width = null,
+                    aspectRatio = 0.743f
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MediaListContent(
+    media: LazyPagingItems<SeeAllScreenState.MediaUiState>,
+    listener: SeeAllInteractionsListener,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(bottom = 16.dp)
+    ) {
+        items(media.itemCount) { index ->
+            media[index]?.let { mediaItem ->
+                TrendingMovieCard(
+                    modifier = Modifier.clickable {
+                        listener.onClickMedia(mediaItem.id, mediaItem.isMovie)
+                    },
+                    imgUrl = mediaItem.posterPath,
+                    rating = String.format(
+                        Locale.getDefault(), "%.1f", mediaItem.rating
+                    ),
+                    movieTitle = mediaItem.title,
+                    movieCategory = mediaItem.genres.firstOrNull()?.name.orEmpty()
+                )
             }
         }
     }
