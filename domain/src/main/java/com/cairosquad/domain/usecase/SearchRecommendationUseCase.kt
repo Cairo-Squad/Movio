dcc8e39ae1dfd8989e02763c757c2752e20c1599
Menna Srour <mennasrour441@gmail.com>
2025-08-01 00:54:18 +0300

    fix: add dagger inject to Recommendation search use case

--- domain/src/main/java/com/cairosquad/domain/usecase/SearchRecommendationUseCase.kt
+++ domain/src/main/java/com/cairosquad/domain/usecase/SearchRecommendationUseCase.kt
@@ -1,8 +1,9 @@
 package com.cairosquad.domain.usecase
 
 import com.cairosquad.domain.repository.SearchRecommendationRepository
+import javax.inject.Inject
 
-class GetSearchRecommendationUseCase(
+class GetSearchRecommendationUseCase @Inject constructor(
     private val searchRecommendationRepository: SearchRecommendationRepository
 ) {
     suspend operator fun invoke(query: String): List<String> {
