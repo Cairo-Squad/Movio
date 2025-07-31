478b3f3565564453f9e2e481c00362b4f1fb5d06
gehadepada <gehadepada@gmail.com>
2025-07-31 16:34:28 +0300

    refactor : tab row to use string from string resource

--- design_system/src/main/java/com/cairosquad/design_system/basic_component/TabRow.kt
+++ design_system/src/main/java/com/cairosquad/design_system/basic_component/TabRow.kt
@@ -13,12 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
@@ -64,7 +63,7 @@ fun TabRow(
 
 
     val selectedTabTitle = tabs.getOrNull(selectedTabIndex)
-    val indicatorBrush = if (selectedTabTitle == "Categories") {
+    val indicatorBrush = if (selectedTabTitle ==  stringResource(com.cairosquad.design_system.R.string.categories)) {
         Theme.color.gradiant.horizontalCategoriesGradient
     } else {
         Theme.color.gradiant.horizontalGradient
@@ -137,7 +136,7 @@ fun TabRow(
 private fun TabRowPreview() {
     MovioTheme(isDarkTheme = true) {
         val tabs = listOf("All", "Movies", "Series", "Categories")
-        var selectedTabIndex by remember { mutableIntStateOf(3) } 
+        var selectedTabIndex by remember { mutableIntStateOf(3) }
         Box(
             Modifier
                 .background(Theme.color.surfaces.surface)
