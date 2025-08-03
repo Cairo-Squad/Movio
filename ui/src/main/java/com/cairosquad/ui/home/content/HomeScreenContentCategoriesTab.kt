1380785c157d207b0e03fa8ffce0fc952e1159aa
Elsayed Magdy <saikoo1239@gmail.com>
2025-08-03 15:49:42 +0300

    refactor : edit in categories layout

--- ui/src/main/java/com/cairosquad/ui/home/content/HomeScreenContentCategoriesTab.kt
+++ ui/src/main/java/com/cairosquad/ui/home/content/HomeScreenContentCategoriesTab.kt
@@ -2,21 +2,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,8 +36,6 @@ fun HomeScreenContentCategoriesTab(
     listener: HomeInteractionsListener,
     modifier: Modifier = Modifier
 ) {
-    val scrollState = rememberScrollState()
-
     val media = screenState.categoriesMedia.collectAsLazyPagingItems()
     Box(modifier = modifier.fillMaxSize()) {
         Box(
@@ -50,56 +46,60 @@ fun HomeScreenContentCategoriesTab(
                 .blur(263.85.dp, edgeTreatment = BlurredEdgeTreatment.Unbounded)
                 .background(Theme.color.surfaces.onSurfaceAt5, shape = CircleShape)
         )
-        Column(
+
+        LazyVerticalGrid(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(scrollState)
+                .padding(horizontal = 12.dp),
+            columns = GridCells.Adaptive(minSize = 101.33.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(bottom = 16.dp)
         ) {
 
-            CategoriesChips(
-                modifier = Modifier.padding(top = 16.dp),
-                categories = listOf(stringResource(R.string.genre_all)) + screenState.genres.drop(
-                    1
-                ).map { it.name },
-                selectedChipIndex = screenState.selectedGenreIndex,
-                onChipSelected = { index ->
-                    listener.onGenreSelected(index)
-                })
-            CategoriesChips(
-                modifier = Modifier.padding(top = 12.dp, bottom = 24.dp),
-                categories = HomeScreenState.SortingType.entries.map { stringResource(it.titleId) },
-                selectedChipIndex = screenState.selectedSortingType.ordinal,
-                onChipSelected = { index ->
-                    listener.onSortingSelected(HomeScreenState.SortingType.entries[index])
-                })
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    CategoriesChips(
+                        categories = listOf(stringResource(R.string.genre_all)) +
+                                screenState.genres.drop(1).map { it.name },
+                        selectedChipIndex = screenState.selectedGenreIndex,
+                        onChipSelected = { index ->
+                            listener.onGenreSelected(index)
+                        }
+                    )
+                }
+            }
 
-            LazyVerticalGrid(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(max = 700.dp)
-                    .padding(horizontal = 16.dp),
-                columns = GridCells.Adaptive(minSize = 101.33.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                contentPadding = PaddingValues(bottom = 12.dp),
-                userScrollEnabled = false
-            ) {
-                items(media.itemCount) { index ->
-                    media[index]?.let { moreRecommended ->
-                        MovieCard(
-                            modifier = Modifier.clickable {
-                                listener.onClickMedia(
-                                    moreRecommended.id, moreRecommended.isMovie
-                                )
-                            },
-                            title = moreRecommended.title,
-                            vote = moreRecommended.rating,
-                            imgUrl = moreRecommended.posterPath,
-                            width = null,
-                            aspectRatio = 0.743f
-                        )
-                    }
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    CategoriesChips(
+                        categories = HomeScreenState.SortingType.entries.map { stringResource(it.titleId) },
+                        selectedChipIndex = screenState.selectedSortingType.ordinal,
+                        onChipSelected = { index ->
+                            listener.onSortingSelected(HomeScreenState.SortingType.entries[index])
+                        }
+                    )
+                }
+            }
 
+            items(media.itemCount) { index ->
+                media[index]?.let { moreRecommended ->
+                    MovieCard(
+                        modifier = Modifier.clickable {
+                            listener.onClickMedia(moreRecommended.id, moreRecommended.isMovie)
+                        },
+                        title = moreRecommended.title,
+                        vote = moreRecommended.rating,
+                        imgUrl = moreRecommended.posterPath,
+                        width = null,
+                        aspectRatio = 0.743f
+                    )
                 }
             }
         }
