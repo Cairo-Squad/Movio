d751a3688d40ceae8b838e756591abac230272e8
Zyad Hammad <zyad.nasser@systemira.net>
2025-08-07 10:11:48 +0300

    feature: finish onboarding design

--- ui/src/main/java/com/cairosquad/ui/onboarding/OnboardingScreen.kt
+++ ui/src/main/java/com/cairosquad/ui/onboarding/OnboardingScreen.kt
@@ -3,21 +3,25 @@ package com.cairosquad.ui.onboarding
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.cairosquad.design_system.basic_component.Text
 import com.cairosquad.design_system.theme.MovioTheme
+import com.cairosquad.design_system.theme.Theme
 import com.cairosquad.ui.R
 import com.cairosquad.ui.movio_component.SwitchToStartButton
 import com.cairosquad.ui.utils.ObserveAsEffect
@@ -54,17 +58,55 @@ private fun OnboardingContent(
             .verticalScroll(rememberScrollState())
             .padding(top = 105.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Image(
-            painter = painterResource(R.drawable.im_onboarding),
-            contentDescription = null,
-            contentScale = ContentScale.FillHeight,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(400.dp)
-                .padding(bottom = 40.dp)
-        )
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 12.dp),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.im_onboarding),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 40.dp)
+            )
+
+            Row {
+                Text(
+                    text = stringResource(R.string.open_with_space_end),
+                    style = Theme.textStyle.display.mediumMedium20,
+                    color = Theme.color.surfaces.onSurface,
+                )
+
+                Text(
+                    text = stringResource(R.string.movio),
+                    style = Theme.textStyle.display.largeBold24.copy(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Theme.color.brand.onPrimary,
+                                Theme.color.brand.primary
+                            )
+                        )
+                    ),
+                )
+            }
+
+            Text(
+                text = stringResource(R.string.and_let_the_noise_fade_away),
+                style = Theme.textStyle.display.mediumMedium20,
+                color = Theme.color.surfaces.onSurface,
+                modifier = Modifier.padding(bottom = 12.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.swipe_pick_dive_in_and_be_the_star_of_the_scene),
+                style = Theme.textStyle.label.smallRegular12,
+                color = Theme.color.surfaces.onSurfaceContainer,
+            )
+        }
 
         SwitchToStartButton(
             modifier = Modifier
