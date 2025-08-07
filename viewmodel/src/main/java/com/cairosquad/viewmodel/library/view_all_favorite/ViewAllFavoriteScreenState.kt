225d1ce5bace452033cdd4d2e0ce44a9031fa010
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-08-08 00:50:04 +0300

    remove paging from favorites

--- viewmodel/src/main/java/com/cairosquad/viewmodel/library/view_all_favorite/ViewAllFavoriteScreenState.kt
+++ viewmodel/src/main/java/com/cairosquad/viewmodel/library/view_all_favorite/ViewAllFavoriteScreenState.kt
@@ -1,9 +1,6 @@
 package com.cairosquad.viewmodel.library.view_all_favorite
 
-import androidx.paging.PagingData
 import com.cairosquad.viewmodel.exception.ErrorStatus
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 data class ViewAllFavoriteScreenState(
     val screenStatus: SectionStatus = SectionStatus.LOADING,
@@ -11,9 +8,9 @@ data class ViewAllFavoriteScreenState(
 
     val isRefreshing: Boolean = false,
 
-    val movies: Flow<PagingData<MovieUiState>> = flowOf(PagingData.empty()),
+    val movies: List<MovieUiState> = emptyList(),
     val deletedMoviesIds: List<Long> = emptyList(),
-    val series: Flow<PagingData<SeriesUiState>> = flowOf(PagingData.empty()),
+    val series: List<SeriesUiState> = emptyList(),
     val deletedSeriesIds: List<Long> = emptyList(),
 
     ) {
