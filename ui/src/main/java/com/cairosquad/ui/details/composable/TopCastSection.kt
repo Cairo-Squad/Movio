b1ae879817b9cabb44bc00f76c5cbcd149ef464f
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-08-14 11:56:21 +0300

    fix top cast ripple effect and shape

--- ui/src/main/java/com/cairosquad/ui/details/composable/TopCastSection.kt
+++ ui/src/main/java/com/cairosquad/ui/details/composable/TopCastSection.kt
@@ -1,9 +1,9 @@
 package com.cairosquad.ui.details.composable
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -16,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import com.cairosquad.design_system.R
 import com.cairosquad.ui.movio_component.SectionHeader
 import com.cairosquad.ui.movio_component.SmallArtistCard
+import com.cairosquad.ui.utils.noRippleClickable
 import com.cairosquad.viewmodel.details.movie.MovieScreenState
 import com.cairosquad.viewmodel.details.series.SeriesDetailsScreenState
 
@@ -38,7 +39,9 @@ fun SeriesTopCastSection(
     ) {
         items(cast) {
             SmallArtistCard(
-                modifier = Modifier.clickable { onArtistClicked(it.id) },
+                modifier = Modifier
+                    .noRippleClickable { onArtistClicked(it.id) }
+                    .width(86.dp),
                 name = it.name,
                 imgUrl = it.photoPath
             )
@@ -66,7 +69,9 @@ fun MovieTopCastSection(
     ) {
         items(cast) {
             SmallArtistCard(
-                modifier = Modifier.clickable { onArtistClicked(it.id) },
+                modifier = Modifier
+                    .noRippleClickable { onArtistClicked(it.id) }
+                    .width(86.dp),
                 name = it.name,
                 imgUrl = it.photoPath
             )
