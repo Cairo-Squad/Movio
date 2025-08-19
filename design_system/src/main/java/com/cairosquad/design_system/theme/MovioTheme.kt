f5bb7f4e9ff660857cb3b25e4cf14c9142513d50
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-08-19 14:11:21 +0300

    fix issue with nav bar color in api 26

--- design_system/src/main/java/com/cairosquad/design_system/theme/MovioTheme.kt
+++ design_system/src/main/java/com/cairosquad/design_system/theme/MovioTheme.kt
@@ -1,11 +1,14 @@
 package com.cairosquad.design_system.theme
 
+import android.os.Build
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.cairosquad.design_system.color.LocalMovioColor
 import com.cairosquad.design_system.color.darkThemeColors
@@ -19,7 +22,6 @@ fun MovioTheme(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    UpdateStatusBarIconsForTheme(isDarkTheme)
     val theme = if (isDarkTheme) darkThemeColors else lightThemeColors
 
     CompositionLocalProvider(
@@ -27,6 +29,7 @@ fun MovioTheme(
         LocalMovioTextStyle provides defaultTextStyle,
         LocalIsDark provides isDarkTheme
     ) {
+        UpdateStatusBarIconsForTheme()
         content()
     }
 }
@@ -34,11 +37,21 @@ fun MovioTheme(
 internal val LocalIsDark = staticCompositionLocalOf { true }
 
 @Composable
-private fun UpdateStatusBarIconsForTheme(darkTheme: Boolean) {
-    val isDarkIcons = !darkTheme
+private fun UpdateStatusBarIconsForTheme() {
     val view = LocalView.current
     val window = (view.context as? ComponentActivity)?.window ?: return
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        window.setNavigationBarContrastEnforced(false)
+    }
+
     WindowInsetsControllerCompat(window, view).apply {
-        isAppearanceLightStatusBars = isDarkIcons
+        isAppearanceLightStatusBars = !Theme.isDark
+        isAppearanceLightNavigationBars = !Theme.isDark
+    }
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.navigationBarColor = Theme.color.surfaces.surface.toArgb()
     }
 }
